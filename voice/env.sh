3e25770bda41708e232a2539f6bc910c2dac7207
Sam Watkins <sam@ucm.dev>
2023-04-06 18:31:42 +1000

    readme

--- voice/env.sh
+++ voice/env.sh
@@ -10,6 +10,6 @@ rewind=2
 
 speak="speak.py --model coqui:tts_models/en/ek1/tacotron2 --tempo 1.0 --pitch 4"
 #speak="speak.py --model gtts:en:co.uk --tempo 1.3 --pitch 3"
-#speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC_ph --tempo 1.2 --pitch 5"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC_ph --tempo 1.4 --pitch 7"
 #speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC --tempo 1.0 --pitch 4"
 #speak="speak.py --model coqui:tts_models/en/ljspeech/glow-tts --tempo 1.2 --pitch 4"
