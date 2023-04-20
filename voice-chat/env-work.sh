925cc818c61e0d3b15fa7f820deba48ef0a55114
Sam Watkins <sam@ucm.dev>
2023-04-20 12:33:30 +1000

    .

---
+++ voice-chat/env-work.sh
@@ -0,0 +1,13 @@
+CHATPATH=$HOME/chat
+user=${USER^}
+bot=Sofie
+file=$HOME/chat/$user-$bot.bb
+mission="system: You are friends and co-workers talking face to face and working enthusiastically on developing an AI toolkit."
+add_prompts=   # 1
+rewind=2
+
+#speak="speak.py --model coqui:tts_models/en/ek1/tacotron2 --tempo 1.2 --pitch 4"
+speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC_ph --tempo 1.3 --pitch 3"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC --tempo 1.1 --pitch 3"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/glow-tts --tempo 1.2 --pitch 4"
+#speak="speak.py --model gtts:en:co.uk --tempo 1.3 --pitch -1"
