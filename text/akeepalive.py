5223c09802526da43e34d1e09c3638709034a830
Sam Watkins <sam@ucm.dev>
2023-04-22 16:30:04 +1000

    improve akeepalive

--- text/akeepalive.py
+++ text/akeepalive.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" atimeout: Async Timeout Iterator """
+""" akeepalive: Async Keepalive Generator """
 
 import sys
 import argparse
@@ -15,37 +15,44 @@ logger = logging.getLogger(__name__)
 async def async_keepalive(iterable, timeout, timeout_return=None):
 	""" Async Keepalive Generator """
 
-	iterator = iterable.__aiter__()
-	timeout_task = next_item_task = None
+	iterator = aiter(iterable)
+	next_item_task = None
+	timeout_task = None
 
 	while True:
 		if timeout_task is None:
 			timeout_task = asyncio.create_task(asyncio.sleep(timeout))
 		if next_item_task is None:
-			next_item_task = asyncio.create_task(iterator.__anext__())
+			next_item_task = asyncio.create_task(anext(iterator))
 
-		done, pending = await asyncio.wait(
+		done, _ = await asyncio.wait(
 			{next_item_task, timeout_task},
 			return_when=asyncio.FIRST_COMPLETED,
 		)
 
-		if len(done) != 1:
-			raise RuntimeError("Unexpected number of completed tasks")
+		assert len(done) == 1
 
-		if next_item_task in done:
-			next_item_task = None
-			timeout_task.cancel()
+		task = done.pop()
+
+		if task == timeout_task:
 			timeout_task = None
-		else:
-			assert timeout_task in done
-			timeout_task = None
+			yield timeout_return
+			continue
+
+		assert task == next_item_task
+
+		next_item_task = None
+		timeout_task.cancel()
+		timeout_task = None
 
 		try:
-			yield done.pop().result()
+			yield task.result()
 		except StopAsyncIteration:
-			for task in pending:
-				task.cancel()
 			break
+
+	for task in next_item_task, timeout_task:
+		if task:
+			task.cancel()
 
 
 async def async_keepalive_demo(timeout, timeout_return):
@@ -53,17 +60,14 @@ async def async_keepalive_demo(timeout, timeout_return):
 	async with aiofiles.open(sys.stdin.fileno(), mode='r') as iterable:
 		async_iterable = async_keepalive(iterable, timeout, timeout_return)
 		async for item in async_iterable:
-			if item is None:
-				print("Timeout")
-			else:
-				print(item, end='')
+			print(item, end='', flush=True)
 
 
 def get_opts():
 	""" Get command line options """
 	parser = argparse.ArgumentParser(description="atimeout", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-t', '--timeout', type=float, default=1, help="Timeout value in seconds")
-	parser.add_argument('-r', '--timeout-return', default=None, help="Value to return on timeout")
+	parser.add_argument('-r', '--timeout-return', default=".", help="Value to return on timeout")
 	ucm.add_logging_options(parser)
 	opts = parser.parse_args()
 	return opts
