97ba530772786618caa812c60f69e150f6240662
Sam Watkins <sam@ucm.dev>
2023-04-27 23:18:30 +1000

    cv2 video scripts WIP, minor fixes

--- voice-chat/speak.sh
+++ voice-chat/speak.sh
@@ -7,6 +7,14 @@ if [ ! -e "$file" ]; then
 	> "$file"
 fi
 
+if [ -n "`amixer sget Capture | grep '\[off\]'`" ]; then
+	mic_state=nocap
+else
+	mic_state=cap
+fi
+
+trap "amixer sset Capture $mic_state; pkill -P $$" EXIT
+
 atail.py -f -r -n"${rewind:-0}" "$file" |
 perl -ne '
 	chomp;
