b196abafa71beaab10ae192ca13d4a017041134d
Sam Watkins <sam@ucm.dev>
2023-04-29 22:07:28 +1000

    voice filter fixes

--- voice-chat/filter-speech.pl
+++ voice-chat/filter-speech.pl
@@ -25,10 +25,12 @@ sub to_letters {
 	$word =~ s/(.)/$1./g;
 	return $word;
 }
-s/[-–—]/.../g;
+# s/[-–—…]/.../g;
+s/[-–—…]/. /g;
 s/’/'/g;
 s/[^\0-~]//g;    # filter out emojis; but \x7e9 is closing single-quote / "smart" apostrophe
 #s{\bThalia\b}{<phoneme ph="təˈliːə">Tahlia</phoneme>}g;
-s{\bThalia\b}{Tahlia}g;
+s{\bThalia\b}{Tah-leeia}g;
+s{\bkinda\b}{kind-a}g;
 s/\b([A-Z]{2,})\b/$letter_by_letter{$1} ? to_letters($1) : ucfirst(lc $1)/ge;
 s/\bDr ([A-Z])/Doctor $1/g;
