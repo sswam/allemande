e6115609dc17214214eaab27280f874ec7c4b218
Sam Watkins <sam@ucm.dev>
2023-05-03 16:08:18 +1000

    test claude coding abilities

--- anthropic/test-claude-code.py
+++ anthropic/test-claude-code.py
@@ -1,46 +1,53 @@
 #!/usr/bin/env python3
 
-# Demonstrate Python basics and new features
+# Demonstrate Python basics and new features  
 
-# Use f-string for string interpolation 
-name = "John"
-print(f"Hello, {name}!") 
+# Use f-string for string interpolation  
+name = "John"  
+print(f"Hello, {name}!")  
 
-# Use a list comprehension
-squares = [x*x for x in range(10)]
+# Use a list comprehension  
+squares = [x*x for x in range(10)]  
+print(squares)  # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
-# Use a dictionary comprehension
+# Use a dictionary comprehension 
 square_dict = {x: x*x for x in range(10)}
+print(square_dict)  # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}  
 
 # Use a set comprehension
 square_set = {x*x for x in range(10)}
+print(square_set)  # {0, 1, 4, 9, 16, 25, 36, 49, 64, 81}
 
-# Define a function with a default argument 
+# Define a function with a default argument
 def add(a=10):
-    return a + a 
+    return a + a
+print(add()) # 20
+print(add(5)) # 10 
 
-# Use functools for partial application
+# Use functools for partial application 
 from functools import partial
 add_five = partial(add, 5)
+print(add_five()) # 10
 
-# Use a namedtuple 
-from collections import namedtuple
-Point = namedtuple("Point", "x y")
+# Use a namedtuple  
+from collections import namedtuple  
+Point = namedtuple("Point", "x y")  
 p = Point(x=10, y=20)
+print(p) # Point(x=10, y=20)
 
-# Use a Counter from the Collections module 
-from collections import Counter
-c = Counter([1,1,1,2,2,3])
+# Use a Counter from the Collections module  
+from collections import Counter  
+c = Counter([1,1,1,2,2,3])  
+print(c) # Counter({1: 3, 2: 2, 3: 1})
 
-# Use numpy for fast array processing
+# Use numpy for fast array processing 
 import numpy as np 
 a = np.array([1, 2, 3]) 
 b = np.array([4, 5, 6])
-c = a + b 
-print(c) 
-# [5 7 9]
+c = a + b  
+print(c) # [5 7 9] 
 
-# Use a context manager to time a block of code
+# Use a context manager to time a block of code 
 import time
 
 # Timer context manager 
@@ -58,6 +65,6 @@ class Timer:
     def __repr__(self):
         return f"{self.duration()} seconds"
 
-with Timer() as t:
+with Timer() as t:    
     time.sleep(1)
-print(f"Sleep time: {t}")
+print(f"Sleep time: {t}") # Sleep time: 1.0 seconds
