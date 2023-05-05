4d5f391354e5a68347521faec0b8ec0959418b99
Sam Watkins <sam@ucm.dev>
2023-05-06 07:33:33 +1000

    Update PYTHONPATH
    - add anthropic folder to PYTHONPATH
    - use loop

--- env.sh
+++ env.sh
@@ -22,7 +22,9 @@ fi
 
 PYTHON=$(which python3)
 
-PYTHONPATH=$ALLEMANDE_HOME/py:$ALLEMANDE_HOME/text:$ALLEMANDE_HOME/chat:$PYTHONPATH
+for dir in py text chat anthropic; do
+	PYTHONPATH=$PYTHONPATH:$ALLEMANDE_HOME/$dir
+done
 
 for dir in adm core sys tools text data image audio video code openai anthropic web chat voice-chat eg www html; do
 	PATH=$PATH:$ALLEMANDE_HOME/$dir
