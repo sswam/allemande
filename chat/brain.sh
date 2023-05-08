b545c7aa17b181e17a0a811e0becf902be1b26c6
Sam Watkins <sam@ucm.dev>
2023-05-08 16:52:21 +1000

    Add one commit message line not exceeding 50 chars: Rename llama-chat-4.py to llama-chat.py

--- chat/brain.sh
+++ chat/brain.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eua
 
-v llama-chat.py -v -m "$LLM_MODEL" -w "$CHATPATH" -c "$ALLEMANDE_HOME/config/llm_llama/experiment.yaml" --delim $'\n' -u "$user" -b "$bot" -n "$TOKEN_LIMIT" --ignore-shrink --ignore "$user:"
+v llama-chat.py -v -m "$LLM_MODEL" -w "$CHATPATH" -c "$ALLEMANDE_HOME/config/llm_llama/experiment.yaml" \
+	--delim $'\n' -u "$user" -b "$bot" -n "$TOKEN_LIMIT" --ignore-shrink --ignore "$user:" --get-roles-from-history "$@"
 
 # not using options: -r --no-trim
