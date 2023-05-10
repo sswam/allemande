72e1ae047c7ccd50712b9f8b3fb7cd2da92ff067
Sam Watkins <sam@ucm.dev>
2023-05-11 05:04:52 +1000

    feat(gradio): Add more functionality to hello.py
    - Add support for taking in additional inputs for morning/evening and
    temperature.
    - Update the interface to include text, checkbox, and slider inputs, with
    corresponding outputs.
    - Change the executable file permission from 644 to 755.

--- gradio/hello.py
+++ gradio/hello.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+os.environ["GRADIO_ANALYTICS"] = "False"
 import gradio as gr
 
-def greet(name):
+def greet1(name):
 	return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+def greet(name, is_morning, temperature):
+	salutation = "Good morning" if is_morning else "Good evening"
+	greeting = f"{salutation}, {name}! It is {temperature} degrees out right now."
+	farenheit = temperature * 9/5 + 32
+	return greeting, round(farenheit, 2)
 
-demo.launch()
+# text, image, audio, video, sketchpad, or label
+
+#demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+#demo = gr.Interface(fn=greet, inputs=gr.Textbox(lines=2, placeholder="Name here"), outputs="text")
+demo = gr.Interface(fn=greet, inputs=["text", "checkbox", gr.Slider(0,100)],
+	outputs=["text", "number"], allow_flagging='never')
+
+if __name__ == "__main__":
+	demo.launch()
