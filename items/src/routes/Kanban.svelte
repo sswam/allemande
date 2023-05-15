c22fc12dab731a4039d0e1900b407156e6247f3d
Sam Watkins <sam@ucm.dev>
2023-05-15 12:01:37 +1000

    feat: add draggable items and error handling in API request
    - Make items draggable and apply dragStart function in Item.svelte
    - Implement fetch error handling in Kanban.svelte
    - Add drag item drop handling to change status in Kanban.svelte

--- items/src/routes/Kanban.svelte
+++ items/src/routes/Kanban.svelte
@@ -5,14 +5,36 @@
 
   onMount(async () => {
     const response = await fetch('/api/items');
-    const items = await response.json();
-    items.set(items);
+    if (response.ok) {
+      const items = await response.json();
+      items.set(items);
+    } else {
+      console.error(response.status);
+    }
   });
 //  let items = [
 //    { id: 1, status: 'todo', subject: 'Item 1' },
 //    { id: 2, status: 'doing', subject: 'Item 2' },
 //    { id: 3, status: 'done', subject: 'Item 3' }
 //  ];
+
+  let draggedItem = null;
+
+  function dragStart(event, item) {
+    draggedItem = item;
+  }
+
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+
+  function drop(event, status) {
+    event.preventDefault();
+    draggedItem.status = status;
+
+    // TODO: Call server function to update item
+  }
+
 </script>
 
 <style>
@@ -33,21 +55,21 @@
 </style>
 
 <div class="kanban-board">
-  <div class="kanban-column">
+  <div class="kanban-column" ondrop="{event => drop(event, 'todo')}" ondragover="{allowDrop}">
     <h2>To Do</h2>
     {#each $items.filter(item => item.status === 'todo') as item}
       <Item {item} />
     {/each}
   </div>
 
-  <div class="kanban-column">
+  <div class="kanban-column" ondrop="{event => drop(event, 'doing')}" ondragover="{allowDrop}">
     <h2>Doing</h2>
     {#each $items.filter(item => item.status === 'doing') as item}
       <Item {item} />
     {/each}
   </div>
 
-  <div class="kanban-column">
+  <div class="kanban-column" ondrop="{event => drop(event, 'done')}" ondragover="{allowDrop}">
     <h2>Done</h2>
     {#each $items.filter(item => item.status === 'done') as item}
       <Item {item} />
