8aad8191efb3cc823bfca28a9591e688528823ef
Sam Watkins <sam@ucm.dev>
2023-05-25 02:16:46 +1000

    feat: add username prefix to remote_messages content
    - Change `u = u.lower()` to `u_lc = u.lower()` for consistency
    - Add content variable to store msg["content"]
    - Append username to content if role is "user"

--- chat/llama-chat.py
+++ chat/llama-chat.py
@@ -655,15 +655,18 @@ def remote_agent(agent, query, file, args, history, history_start=0):
 		for msg in context_messages:
 			logger.warning(msg)
 			u = msg.get("user")
-			u = u.lower() if u is not None else None
+			u_lc = u.lower() if u is not None else None
 #			if u in agents_lc:
-			if u == agent['name'].lower()
+			content = msg["content"]
+			if u_lc == agent['name'].lower():
 				role = "assistant"
 			else:
 				role = "user"
+				if u:
+					content = u + ":\t" + content
 			msg2 = {
 				"role": role,
-				"content": msg["content"],
+				"content": content,
 			}
 			logger.warning("msg: %r", msg2)
 			remote_messages.append(msg2)
