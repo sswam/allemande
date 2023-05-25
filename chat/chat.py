3174349cadedfa3bb231737132f1242f592d3750
Sam Watkins <sam@ucm.dev>
2023-05-26 05:00:13 +1000

    fix(chat): remove unnecessary flag and commented code
    - Removed 'first' flag as it's not used in the code
    - Commented out redundant line checking for html tags

--- chat/chat.py
+++ chat/chat.py
@@ -263,12 +263,13 @@ def preprocess(content):
 		return f"${math}$"
 
 	out = []
-	first = True
 
 	in_math = False
-	is_html = False
 	for line in content.splitlines():
-		if first and re.search(r"\t<", line[0]):
+		is_html = False
+		#if first and re.search(r"\t<", line[0]):
+		#	is_html = True
+		if re.search(r"</?(html|base|head|link|meta|style|title|body|address|article|aside|footer|header|h1|h2|h3|h4|h5|h6|hgroup|main|nav|section|blockquote|dd|div|dl|dt|figcaption|figure|hr|li|main|ol|p|pre|ul|a|abbr|b|bdi|bdo|br|cite|code|data|dfn|em|i|kbd|mark|q|rb|rp|rt|rtc|ruby|s|samp|small|span|strong|sub|sup|time|u|var|wbr|area|audio|img|map|track|video|embed|iframe|object|param|picture|source|canvas|noscript|script|del|ins|caption|col|colgroup|table|tbody|td|tfoot|th|thead|tr|button|datalist|fieldset|form|input|label|legend|meter|optgroup|option|output|progress|select|textarea|details|dialog|menu|summary|slot|template|acronym|applet|basefont|bgsound|big|blink|center|command|content|dir|element|font|frame|frameset|image|isindex|keygen|listing|marquee|menuitem|multicol|nextid|nobr|noembed|noframes|plaintext|shadow|spacer|strike|tt|xmp)\b", line):
 			is_html = True
 		if not is_html:
 			line = html.escape(line)
