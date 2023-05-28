7f8eb6424a6ac0facce365d865f099fdb96cf062
Sam Watkins <sam@ucm.dev>
2023-05-28 23:40:32 +1000

    feat(wordpress_add): add script to create WordPress pages or posts from HTML files
    - add new python script file named wordpress_add.py
    - define a new function wordpress_add for the creation of WordPress
    pages/posts
    - use environment variables and command-line arguments for required
    credentials
    - implement HTTP Basic Auth for API request
    - load HTML content from specified file, parse title if not provided
    - send API request to create a page/post using provided information
    - print the success or error message based on the API response
    - use argh for argument parsing and script dispatching

---
+++ www/wordpress_add.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3 
+
+import os
+import requests
+import argh
+
+# TODO look for an existing Python API to wordpress, I'm sure there is one!
+# wordpress-api:
+# - https://pypi.org/project/wordpress-api/
+# - https://github.com/d3v-null/wp-api-python
+
+@argh.arg("html_file", help="HTML file to upload")
+@argh.arg("--title", help="Title of the page")
+@argh.arg("--status", help="Status of the page (draft, publish, private)")
+@argh.arg("--type", help="Type of the page (page, post)")
+@argh.arg("--site-url", help="URL of the WordPress site")
+@argh.arg("--username", help="Username for the WordPress site")
+@argh.arg("--app-password", help="Application password for the WordPress site")
+def wordpress_add(
+	html_file, title=None, status="draft", type="page",
+	site_url=os.environ.get("WORDPRESS_SITE_URL"),
+	username=os.environ.get("WORDPRESS_USERNAME"),
+	app_password=os.environ.get("WORDPRESS_APP_PASSWORD"),
+):
+	""" Create a new page or post in WordPress from an HTML file """
+
+	# Full API endpoint for creating pages or posts
+	endpoint = "pages" if type == "page" else "posts"
+	api_url = f"{site_url}/wp-json/wp/v2/{endpoint}"
+
+	# Set up the HTTP Basic Auth object
+	auth = requests.auth.HTTPBasicAuth(username, app_password)
+
+	# Load the HTML file
+	with open(html_file, "r") as file:
+		html_content = file.read()
+
+	# Get title from first line of page, if not provided
+	if not title:
+		title = html_content.split("\n")[0]
+		title = title.replacer("<.*?>", "")
+		title = title.replacer("Title: ", "")
+
+	# Set up the payload to send to WordPress, including your content
+	payload = {
+		"content": html_content,
+		"title": title,
+		"status": status,
+	}
+
+	# Send the post request to create a new page, passing the payload and authorization
+	response = requests.post(api_url, json=payload, auth=auth)
+
+	# Check if the request was successful
+	if response.status_code == 201:  # 201 is the status code for 'Created'
+		print("Page created successfully!")
+	else:
+		print("There was an error creating the page:", response.text)
+
+if __name__ == "__main__":
+	argh.dispatch_command(wordpress_add)
