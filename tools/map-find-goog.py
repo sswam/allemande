81c68ca7938340fea8277c91f66431c7ee16d3e5
Sam Watkins <sam@ucm.dev>
2023-05-29 22:51:00 +1000

    feat(tools): improve map-find search functions
    - add docstrings for map-find-goog and map-find-mapbox
    - add timeout argument for search_location in both files
    - refactor search_location to use early return

--- tools/map-find-goog.py
+++ tools/map-find-goog.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
-# map-find.py:	Search for a location using Google Maps API
+""" map-find.py:	Search for a location using Google Maps API """
+
 import os
 import requests
 import argh
 
-def search_location(location, api_key=os.environ.get("GOOGLE_MAPS_API_KEY")):
+def search_location(location, api_key=os.environ.get("GOOGLE_MAPS_API_KEY"), timeout=10):
+	""" Search for a location using Google Maps API """
 	url = "https://maps.googleapis.com/maps/api/geocode/json"
 	params = {
 		"address": location,
 		"key": api_key
 	}
 
-	response = requests.get(url, params=params)
+	response = requests.get(url, params=params, timeout=timeout)
 	data = response.json()
 
-	if data["results"]:
-		result = data["results"][0]
-		lat, lon = (result["geometry"]["location"]["lat"],
-					result["geometry"]["location"]["lng"])
-		address = result["formatted_address"]
-		return lat, lon, address
-	else:
+	if not data["results"]:
 		return None, None, None
+
+	result = data["results"][0]
+	loc = result["geometry"]["location"]
+	lat, lon = (loc["lat"], loc["lng"])
+	address = result["formatted_address"]
+	return lat, lon, address
 
 if __name__ == "__main__":
 	argh.dispatch_command(search_location)
--- tools/map-find-mapbox.py
+++ tools/map-find-mapbox.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
-# map-find.py:	Search for a location using Mapbox API
+""" map-find-mapbox.py:	Search for a location using Mapbox API """
+
 import os
 import requests
 import argh
 
-def search_location(location, access_token=os.environ.get("MAPBOX_API_KEY")):
+def search_location(location, access_token=os.environ.get("MAPBOX_API_KEY"), timeout=10):
+	""" Search for a location using Mapbox API """
 	url = f"https://api.mapbox.com/geocoding/v5/mapbox.places/{location}.json"
 	params = {
 		"access_token": access_token,
 		"limit": 1
 	}
 
-	response = requests.get(url, params=params)
+	response = requests.get(url, params=params, timeout=timeout)
 	data = response.json()
 
-	if data["features"]:
-		feature = data["features"][0]
-		lon, lat = feature["geometry"]["coordinates"]
-		address = feature["place_name"]
-		return lat, lon, address
-	else:
+	if not data["features"]:
 		return None, None, None
+
+	feature = data["features"][0]
+	lon, lat = feature["geometry"]["coordinates"]
+	address = feature["place_name"]
+	return lat, lon, address
 
 if __name__ == "__main__":
 	argh.dispatch_command(search_location)
