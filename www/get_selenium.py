4852d55c78953f4c478a62e8fdb1388009b50376
Sam Watkins <sam@ucm.dev>
2023-05-31 07:00:58 +1000

    fix(get_selenium): update script handling, refactor code and add Facebook option
    - removed unused file "facebook_close_dialog.js"
    - changed file name and docstring style in "get_selenium.py"
    - added importing 'Path' from 'pathlib'
    - modified 'scroll_to_bottom()' to include docstring
    - added arguments and refactored 'get_selenium()' function
    - combined script execution and added Facebook option
    - removed unnecessary 'get_page_content()' function and call

--- www/get_selenium.py
+++ www/get_selenium.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-""" get-selenium.py:	Uses Selenium to fetch the content of a web page. """
+""" get_selenium.py	Uses Selenium to fetch the content of a web page. """
 
+import sys
+import logging
 import time
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import argh
-import sys
-import logging
 
 logger = logging.getLogger(__name__)
 
 
-def scroll_to_bottom(wd, time_limit=30, scroll_limit=100000, scroll_wait=1, retry_each_scroll=3, exe=exe, script_wait=1, retry_script=3):
+def scroll_to_bottom(wd, time_limit=30, scroll_limit=100000, scroll_wait=1, retry_each_scroll=3, exe=None, script_wait=1, retry_script=3):
+    """ Scroll to the bottom of the page. """
     start_time = time.time()
     last_height = wd.execute_script("return document.body.scrollHeight")
     scrolled = 0
@@ -52,23 +54,6 @@ def scroll_to_bottom(wd, time_limit=30, scroll_limit=100000, scroll_wait=1, retr
             break
 
 
-def get_page_content(url, time_limit=30, scroll_limit=100000, scroll_wait=1, retry_each_scroll=3, script=None, exe=None, script_wait=1, retry_script=3, headless=True):
-    opts = webdriver.ChromeOptions()
-    if headless:
-        opts.add_argument("--headless")
-
-    exe = exe or ''
-    if script:
-        exe += open(script).read()
-
-    with webdriver.Chrome(service=Service(), options=opts) as wd:
-        wd.get(url)
-        scroll_to_bottom(wd, time_limit=time_limit, scroll_limit=scroll_limit, scroll_wait=scroll_wait, retry_each_scroll=retry_each_scroll, exe=exe, script_wait=script_wait)
-        page_source = wd.page_source
-
-    return page_source
-
-
 @argh.arg("url", help="URL of the web page to fetch content")
 @argh.arg("--time-limit", '-t', help="Maximum time for scrolling, in seconds", type=int, default=30)
 @argh.arg("--scroll-limit", '-h', help="Maximum number of pixels to scroll down", type=int)
@@ -78,9 +63,30 @@ def get_page_content(url, time_limit=30, scroll_limit=100000, scroll_wait=1, ret
 @argh.arg("--exe", '-e', help="JavaScript to run before scrolling")
 @argh.arg("--script-wait", '-T', help="Time to wait after running script, in seconds", type=int, default=1)
 @argh.arg("--retry-script", '-R', help="Number of times to retry running script", type=int, default=3)
-def get_selenium(url, time_limit=30, scroll_limit=None, scroll_wait=1, retry_each_scroll=3, script=None, exe=None, script_wait=1, retry_script=3):
-    page_content = get_page_content(url, time_limit=time_limit, scroll_limit=scroll_limit, scroll_wait=scroll_wait, retry_each_scroll=retry_each_scroll, script=script, exe=run, script_wait=script_wait, retry_script=retry_script)
-    return page_content
+@argh.arg("--headless", '-H', help="Run in headless mode", action='store_true')
+@argh.arg("--facebook", '-f', help="Download from Facebook", action='store_true')
+def get_selenium(url, time_limit=30, scroll_limit=None, scroll_wait=1, retry_each_scroll=3, script=None, exe=None, script_wait=1, retry_script=3, headless=True, facebook=False):
+    """ Uses Selenium to fetch the content of a web page. """
+    program = Path(sys.argv[0])
+    prog_dir = program.parent
+
+    opts = webdriver.ChromeOptions()
+    if headless:
+        opts.add_argument("--headless")
+
+    exe = exe or ''
+    if script:
+        exe += "\n" + open(script).read() + "\n"
+
+    if facebook:
+        exe += "\n" + (prog_dir/"facebook_scroller.js").read_text() + "\n"
+
+    with webdriver.Chrome(service=Service(), options=opts) as wd:
+        wd.get(url)
+        scroll_to_bottom(wd, time_limit=time_limit, scroll_limit=scroll_limit, scroll_wait=scroll_wait, retry_each_scroll=retry_each_scroll, exe=exe, script_wait=script_wait)
+        page_source = wd.page_source
+
+    return page_source
 
 
 if __name__ == "__main__":
