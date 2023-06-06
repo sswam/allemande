e75dcf4ecf3378b4c9212157aca001db4a16ef22
Sam Watkins <sam@ucm.dev>
2023-06-06 16:35:05 +1000

    feat(cat-sections): add force option to skip unreadable files
    - add logging to warn when skipping files
    - restructure cat_sections function for readability
    - handle FileNotFoundError, IsADirectoryError, PermissionError

--- text/cat-sections.py
+++ text/cat-sections.py
@@ -3,35 +3,56 @@
 import argparse
 import sys
 import re
+import logging
 
 from pathlib import Path
 
-def cat_sections(files, out, sep="\n", keep_filenames=False):
-	for a in files:
-		path = Path(a)
+logger = logging.getLogger(__name__)
+
+def cat_sections(files, out, sep="\n", keep_filenames=False, force=False):
+	good = []
+	for file in files:
+		path = Path(file)
+		try:
+			with open(file, "r") as f:
+				good.append(file)
+		except (FileNotFoundError, IsADirectoryError, PermissionError) as e:
+			if force:
+				logger.warning("Cannot open file, skipping: %r: %r", file, e)
+			else:
+				raise
+
+	for file in good:
+		path = Path(file)
 		name = path.stem
 
 		if not keep_filenames:
 			name = re.sub(r"[-_]+", " ", name)
 			name = name.title()
 
-		print(f"## {name}\n", file=out)
-		with open(a, "r") as f:
-			t = f.read()
-			if t and t[-1] == "\n":
-				t = t[:-1]
-			print(t, file=out)
-
-		print(sep, file=out, end="")
+		try:
+			with open(file, "r") as f:
+				t = f.read()
+				if t and t[-1] == "\n":
+					t = t[:-1]
+				print(f"## {name}\n", file=out)
+				print(t, file=out)
+				print(sep, file=out, end="")
+		except (FileNotFoundError, IsADirectoryError, PermissionError) as e:
+			if force:
+				logger.warning("Cannot open file, skipping: %r: %r", file, e)
+			else:
+				raise
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("files", nargs="*", help="list of files to concatenate")
 	parser.add_argument("--sep", default="\n\n", help="paragraph break character (default: newline)")
 	parser.add_argument("--keep-filenames", "-k", action="store_true", help="don't clean up the filenames in output")
+	parser.add_argument("--force", "-f", action="store_true", help="continue even if some files are unreadable")
 	args = parser.parse_args()
 
-	cat_sections(args.files, sys.stdout, sep=args.sep, keep_filenames=args.keep_filenames)
+	cat_sections(args.files, sys.stdout, sep=args.sep, keep_filenames=args.keep_filenames, force=args.force)
 
 
 if __name__ == "__main__":
