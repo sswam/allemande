36e8a3a413722f7bd4681c601ba1faf5da13c17e
Sam Watkins <sam@ucm.dev>
2023-10-01 01:46:03 +1000

    feat(www): add web-summary-test.sh script for web page analysis
    - The script accepts a web page name and URL as arguments
    - It creates a directory with the given name and downloads the URL content to
    the directory
    - The content is summarised in various ways and using llm tool. The
    outputs are saved as text files
    - Detailed stats about the content and summaries (e.g., char, word, token
    counts) are calculated and reported
    - The output is structured in an organised format with a report.txt and a
    row.tsv file containing the stats written to the directory
    - The script mainly uses bash commands, Perl and wc tool.

---
+++ www/web-summary-test.sh
@@ -0,0 +1,143 @@
+#!/bin/bash -euv
+# web-summary-test: summarise a web page in various ways,
+# and produce stats to help estimate costs
+
+name=$1
+url=$2
+
+mkdir "$name"
+cd "$name"
+
+points_prompt="Please make a comprehensive dot point summary of this text from a web page, in markdown, including the main topic, and all significant points relating to the main topic, up to 20 dot points as needed. Don't include extraneous info about the web page such as navigation or technical details on how the web page works. Thanks for doing a great job!"
+summary_prompt="Please write a 2 or 3 paragraph summary, in markdown, including any important links if given. No more than 250 words. The summary should be clear and easy to read, in fairly simple, concise language for a wide audience, and using fairly short sentences. Please include most of the facts given, and do not add additional facts, opinions or commentary (other than what might be unambiguously implied or well known). Please preserve quotes. Thanks for doing a great job!
+
+Please follow this output format, including the three given headings and replacing the text between square brackets:
+
+# Type:
+
+[the type of content, e.g. News, Article, Product, Company, Information, Personal, Paper, Review, ...]
+
+# Topic:
+
+[the topic, a single phrase or sentence summing up the content]
+
+# Summary:
+
+[the requested summary of the content]
+
+----"
+
+text_cutoff=16000
+
+wg -O=page.html "$url"
+pandoc-dump page.html "$url" | tee page-full.txt
+
+head -c "$text_cutoff" < page-full.txt > page.txt
+
+chars=`wc -c < page.txt | tee page-chars`
+words=`wc -w < page.txt | tee page-words`
+tokens=`llm count < page.txt | tee page-tokens`
+chars_per_token=`calc $chars/$tokens`
+chars_per_word=`calc $chars/$words`
+tokens_per_word=`calc $tokens/$words`
+
+time llm process -m i "$points_prompt" < page.txt |
+perl -e 'undef $/; $_ = <>; s/\A[a-z].*:\n+//is; print' |  # strip any intro heading
+tee points.txt
+
+points_prompt_tokens=`printf "%s\n\n" "$points_prompt" | llm count`
+summary_prompt_tokens=`printf "%s\n\n" "$summary_prompt" | llm count`
+
+points_chars=`wc -c < points.txt | tee points-chars`
+points_words=`wc -w < points.txt | tee points-words`
+points_tokens=`llm count < points.txt | tee points-tokens`
+
+for model in i c 4; do
+	time llm process -m $model "$summary_prompt" < points.txt | tee summary.$model.txt
+	wc -c < summary.$model.txt | tee summary-chars-$model
+	wc -w < summary.$model.txt | tee summary-words-$model
+	llm count < summary.$model.txt | tee summary-tokens-$model
+done
+
+for model in i c; do
+	time llm process -m $model "$summary_prompt" < page.txt | tee summary-one-step.$model.txt
+	wc -c < summary-one-step.$model.txt | tee summary-one-step-chars-$model
+	wc -w < summary-one-step.$model.txt | tee summary-one-step-words-$model
+	llm count < summary-one-step.$model.txt | tee summary-one-step-tokens-$model
+done
+
+header() {
+	local text=$1
+	echo
+	echo
+	echo "$text"
+	echo "$text" | sed 's/./-/g'
+	echo
+}
+
+(
+	header "Info"
+	echo "name: $name"
+	echo "url: $url"
+
+	header "Text"
+	cat page.txt
+
+	header "Points"
+	cat points.txt
+
+	for model in i c 4; do
+		header "Summary ($model)"
+		cat summary.$model.txt
+	done
+
+	for model in i c; do
+		header "Summary One Step ($model)"
+		cat summary-one-step.$model.txt
+	done
+
+	header "Prompt Stats"
+	echo "points_prompt_tokens: $points_prompt_tokens"
+	echo "summary_prompt_tokens: $summary_prompt_tokens"
+
+	header "Text Stats"
+	echo "chars: $chars"
+	echo "words: $words"
+	echo "tokens: $tokens"
+	echo "chars_per_token: $chars_per_token"
+	echo "chars_per_word: $chars_per_word"
+	echo "tokens_per_word: $tokens_per_word"
+
+	header "Points Stats"
+	echo "points_chars: $points_chars"
+	echo "points_words: $points_words"
+	echo "points_tokens: $points_tokens"
+
+	for model in i c 4; do
+		header "Summary Stats ($model)"
+		echo "summary_chars_$model: `cat summary-chars-$model`"
+		echo "summary_words_$model: `cat summary-words-$model`"
+		echo "summary_tokens_$model: `cat summary-tokens-$model`"
+	done
+
+	for model in i c; do
+		header "Summary One Step Stats ($model)"
+		echo "summary_one_step_chars_$model: `cat summary-one-step-chars-$model`"
+		echo "summary_one_step_words_$model: `cat summary-one-step-words-$model`"
+		echo "summary_one_step_tokens_$model: `cat summary-one-step-tokens-$model`"
+	done
+) | tee report.txt
+
+echo
+echo
+
+(
+	printf "%s\t" "$name" "$url" "$points_prompt_tokens" "$summary_prompt_tokens" \
+		"$chars" "$words" "$tokens" "$points_chars" "$points_words" "$points_tokens" \
+		"`cat summary-chars-i`" "`cat summary-words-i`" "`cat summary-tokens-i`" \
+		"`cat summary-chars-c`" "`cat summary-words-c`" "`cat summary-tokens-c`" \
+		"`cat summary-chars-4`" "`cat summary-words-4`" "`cat summary-tokens-4`" \
+		"`cat summary-one-step-chars-i`" "`cat summary-one-step-words-i`" "`cat summary-one-step-tokens-i`" \
+		"`cat summary-one-step-chars-c`" "`cat summary-one-step-words-c`" "`cat summary-one-step-tokens-c`"
+	echo
+) | tee row.tsv
