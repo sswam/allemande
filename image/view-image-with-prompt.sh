74b1f53293f5b30ec22f6000ef4b8453677992d4
Sam Watkins <sam@ucm.dev>
2024-09-16 11:20:34 +1000

    feat(image): add scripts for viewing image parameters and displaying images
    - Add image-params script to extract and format image metadata
    - Create view-image-with-prompt.sh to display images with associated prompts

---
+++ image/view-image-with-prompt.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -eu
+
+x=0
+s=0
+
+. opts
+
+x_screen=$x
+small=$s
+
+if [ -d "${1:-}" ]; then
+	find "$@" -mindepth 1 -maxdepth 1 -name '*.png' | sort | "$0" "${OPTS[@]}"
+	exit $?
+fi
+
+if [[ "${1:-}" == *.* ]]; then
+	lecho "$@" | "$0" "${OPTS[@]}"
+	exit $?
+fi
+
+if [ "$small" = 1 ]; then
+	small=100
+fi
+
+start=${1:-}
+
+if [ -n "$start" ]; then
+	v sed -n "/$start/,\$p"
+else
+	cat
+fi | (
+trap 'exit 1' INT
+while read I; do
+	if [[ "$I" != *.png ]]; then
+		echo "$I"
+		continue
+	fi
+	clear
+	echo "$I"
+	echo
+	image-params "$I" |
+	highlight.py '<[^>]*>' red || true
+
+	if [ "$small" = 0 ]; then
+		qiv --fullscreen --scale_down --xineramascreen ${x_screen:-0} "$I"
+	else
+		qiv -w "$small" --xineramascreen ${x_screen:-1} "$I"
+	fi
+done
+)
