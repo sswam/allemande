e8598df12e2fa62c3c1d3b956dc6d284cbf6eb57
Sam Watkins <sam@ucm.dev>
2024-09-19 06:58:12 +1000

    feat(code): Enhance improve.sh and prog.sh scripts
    - Add -s option for referencing hello.<ext> file for code style
    - Improve file handling and error checking
    - Enhance prompt construction and input processing
    - Refactor common code patterns for consistency
    - Update vimdiff usage and file cleanup in improve.sh

--- code/improve.sh
+++ code/improve.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -eu
+
 # [prog.py] "instructions to improve it" [reference files ...]
 # improve a program using AI
 
 m=c	# model
+s=0	# refer to hello.<ext> for code style
 
 . opts
 
 prog=$1
 prompt=${2-}
-shift 2
+shift 2 || true
 refs=("$@")
 
 if [ ! -e "$prog" ]; then
@@ -20,22 +22,35 @@ if [ ! -e "$prog" ]; then
 	prog=$prog2
 fi
 
-dir=`dirname "$prog"`
-base=`basename "$prog"`
+dir=$(dirname "$prog")
+base=$(basename "$prog")
+ext=.${base##*.}
+if [ "$ext" == ".$base" ]; then
+	ext=""
+fi
+
+# Code style reference and prompt for -s options
+if [ "$s" = 1 ]; then
+	refs+=("hello$ext")
+	prompt="in the style of \`hello$ext\`, $prompt"
+fi
+
+prompt="Please improve \`$base\`, $prompt"
+
+input=$(cat_named.py -p -b "$prog" "${refs[@]}")
 
 (
 	cd "$dir"
-	git add "$base"
-	cp -a "$base" "$base~"
+	if [ -e "$base~" ]; then
+		move-rubbish "$base~"
+	fi
+	yes n | cp -i -a "$base" "$base~"   # WTF, there's no proper no-clobber option?!
 )
 
-prompt="Please improve $base, $prompt"
-
-cat_named.py -p -b "$prog" "${refs[@]}" | process -m="$m" "$prompt" | markdown_code.py -c '#' > "$prog~"
+printf "%s\n" "$input" | process -m="$m" "$prompt" | markdown_code.py -c '#' > "$prog~"
 
 (
 	cd "$dir"
 	swapfiles "$base" "$base~"
 	vimdiff "$base" "$base~"
-	mr "$base~"
 )
--- code/prog.sh
+++ code/prog.sh
@@ -3,21 +3,28 @@
 # write a program using AI
 
 m=c	# model
-h=1	# refer to hello.ext if no other references provided
+s=1	# refer to hello.<ext> for code style
 
 . opts
 
 prog=$1
 prompt=${2-}
-shift 2
+shift 2 || true
+refs=("$@")
 
-ext=${prog##*.}
+if [ -e "$prog" ]; then
+	echo >&2 "already exists: $prog"
+	exit 1
+fi
 
-if [ "$h" = 1 ] && [ $# = 0 ]; then
-	refs=("hello.$ext")
-		prompt="in the style of hello.$ext, $prompt"
-else
-	refs=("$@")
+ext=.${prog##*.}
+if [ "$ext" == ".$base" ]; then
+	ext=""
+fi
+
+if [ "$s" = 1 ]; then
+	refs+=("hello$ext")
+	prompt="in the style of \`hello$ext\`, $prompt"
 fi
 
 dir=`dirname "$prog"`
@@ -25,9 +32,11 @@ base=`basename "$prog"`
 
 mkdir -p "$dir"
 
-prompt="Please write $base, $prompt"
+prompt="Please \`write\` $base, $prompt"
 
-v cat_named.py -p -b "${refs[@]}" | v process -m="$m" "$prompt" | markdown_code.py -c '#' > "$prog"
+input=$(v cat_named.py -p -b "${refs[@]}")
+
+printf "%s\n" "$input" | v process -m="$m" "$prompt" | markdown_code.py -c '#' > "$prog"
 
 chmod +x "$prog"
 vi "$prog"
