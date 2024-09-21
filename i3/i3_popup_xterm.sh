cb0e106bd868afb285219fd7a393107535267661
Sam Watkins <sam@ucm.dev>
2024-09-21 20:04:42 +1000

    feat(i3): enhance i3_popup_xterm script with new options
    - Add hold option to keep terminal open after command execution
    - Implement title setting for terminal window
    - Add wait functionality to block until terminal closes
    - Rename main function from i3_popup_terminal to i3_popup_xterm
    - Reduce sleep time before enabling floating mode

--- i3/i3_popup_xterm.sh
+++ i3/i3_popup_xterm.sh
@@ -3,15 +3,15 @@
 # Run a command in a floating xterm window in i3
 set -e -u -o pipefail
 
-i3_popup_terminal() {
+i3_popup_xterm() {
 	local t="xterm"  # terminal emulator to use
 	local g="80x24"  # geometry of the terminal window
 	local F=0        # flag to disable floating mode
+	local H=0        # flag to hold the terminal open
+	local T=         # title to set for the terminal window
+	local w=	 # wait for the terminal to close
 
-	# Source the opts file if it exists
-	if [[ -f opts ]]; then
-		. opts
-	fi
+	. opts
 
 	local cmd=("$@")
 
@@ -19,6 +19,14 @@ i3_popup_terminal() {
 	local run_cmd=("$t" "-geometry" "$g")
 
 	if [[ ${#cmd[@]} -gt 0 ]]; then
+		# hold?
+		if [ "$H" -eq 1 ]; then
+			run_cmd+=("-hold")
+		fi
+		if [ -n "$T" ]; then
+			run_cmd+=("-T")
+			run_cmd+=("$T")
+		fi
 		run_cmd+=("-e")
 		run_cmd+=("${cmd[@]}")
 	fi
@@ -29,11 +37,16 @@ i3_popup_terminal() {
 
 	# If floating mode is enabled, use i3-msg to make the window floating
 	if [[ $F -eq 0 ]]; then
-		sleep 0.5
+		sleep 0.05
 		i3-msg "floating enable" >/dev/null
+	fi
+
+	# Wait for the terminal to close
+	if [ "$w" -eq 1 ]; then
+		wait
 	fi
 }
 
 if [ "$BASH_SOURCE" = "$0" ]; then
-	i3_popup_terminal "$@"
+	i3_popup_xterm "$@"
 fi
