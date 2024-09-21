f60446cea1b02f10d08291a8d5f30538d6f35392
Sam Watkins <sam@ucm.dev>
2024-09-21 21:04:22 +1000

    feat(plan): add waywo.sh script for work tracking prompts
    - Create new script to periodically ask "What are you working on?"
    - Allow running in foreground or background
    - Provide options to edit waywo file or quit running instance
    - Display prompt using i3 popup and update i3 status message

---
+++ apps/plan/waywo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eu
+#
+# What are you working on?
+
+i=60	# interval
+f=	# Run in the foreground
+q=	# Quit any running waywo
+e=	# Edit the waywo file
+
+. opts
+
+if [ -n "$e" ]; then
+	note -e -t=waywo
+elif [ -z "$f" ]; then
+	v daemonize "$0" -f "${OPTS[@]}" "$@" &
+elif [ -n "$q" ]; then
+	echo "Got quit signal" >&2
+else
+	while true; do
+		i3_popup_xterm -w -T="What are you working on?" note -t=waywo --timeout=60
+		i3_update_message.sh $(note -t=waywo -n -A)
+		sleep "$i" || true
+	done
+fi
