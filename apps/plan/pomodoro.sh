647b3159c21d9660a8124b296db54dbde1b3c6ea
Sam Watkins <sam@ucm.dev>
2024-09-21 21:03:37 +1000

    feat(plan): add Pomodoro timer script
    - Implement basic Pomodoro technique with work, grace, and rest periods
    - Allow customization of timer durations and screen brightness levels
    - Include options for testing, foreground execution, and notifications
    - Integrate with system tools like dunst, xdark, and i3lock
    - Provide automatic restart for continuous Pomodoro cycles

---
+++ apps/plan/pomodoro.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# pomodoro:	Pomodoro timer
+
+work=$[50*60]	# Set the working time
+grace=$[5*60]	# Set the grace time
+rest=$[5*60]	# Set the rest time
+g=	# More grace (or start with rest / grace period)
+q=	# Quit any running pomodoro
+N=	# Take a break now, then continue with next pomodoro
+
+ban="It's time to take a break!"	# Set the banner message
+xd1="0.20 0.75"	# Set the 1st annoy screen brightness
+xd2="0.30 0.50"	# Set the 2nd annoy screen brightness
+xd0="0.00 0.10"	# Set the rest screen brightness
+annoy=0.1	# Set the annoy interval, to switch the screen brightness
+bother=1	# Allow notifications to bother the user during pomodoro; FIXME 0 breaks sleep-log
+n="-n"	# Notify the user of the time remaining, sleep-log opts
+test=	# Use shorter times for testing
+f=	# Run in the foreground
+. opts
+
+# ----------------------------------------------
+# includes and startup message
+
+. log
+. sleep-log
+# . daemonize
+
+log pomodoro timer
+
+# ----------------------------------------------
+
+# use different values if the user is testing
+
+if [ -n "$test" ]; then
+	work=1  # for testing
+	grace=2
+	rest=10
+fi
+
+# ----------------------------------------------
+
+# daemonize the script unless the user wants to run it in the foreground
+
+if [ -z "$f" ]; then
+	v daemonize "$0" -f "${OPTS[@]}" "$@" &
+	exit 0
+fi
+
+# ----------------------------------------------
+
+# reset the screen brightness
+
+function fix_xdark {
+	echo "$(xdark)"
+	case "$(xdark)" in
+	"$xd1"|"$xd2"|"$xd0") xdark 1 ;;
+	esac
+}
+
+fix_xdark
+
+# ----------------------------------------------
+
+# on exit, reset the screen brightness and killall .ban
+
+trap fix_xdark EXIT
+#trap 'killall .ban' EXIT
+
+if [ -n "$q" ]; then
+	echo "Got quit signal" >&2
+	exit 0
+fi
+
+# ----------------------------------------------
+
+# pomodoro - sleep for the pomodoro time
+
+if [ -z "$bother" ]; then
+	dunstctl set-paused true
+fi
+
+if [ -z "$g" -a -z "$N" ]; then
+	log pomodoro
+	sleep-log $n $work 60
+fi
+
+# ----------------------------------------------
+
+# notify the user that the pomodoro is over
+
+log end pomodoro
+
+if [ -z "$bother" ]; then
+	dunstctl set-paused false
+fi
+
+#ban "$ban"
+
+notify-send -u critical -t 10000 \
+	-i /usr/share/icons/gnome/48x48/status/appointment-soon.png \
+	"Pomodoro" "$ban"
+
+if [ -z "$N" ]; then
+	log grace
+	sleep-log $n $grace 5
+fi
+
+# ----------------------------------------------
+
+# annoy the user until they take a break
+
+log annoy
+xd_save=$(xdark)
+while ! q pidof i3lock; do
+	xdark $xd1
+	sleep $annoy
+	if q pidof i3lock; then break; fi
+	xdark $xd2
+	sleep $annoy
+done
+
+# ----------------------------------------------
+
+# sleep for the rest period
+
+xdark $xd0
+sleep-log $rest
+
+# ----------------------------------------------
+
+# wait for the user to unlock the screen again
+
+log wait
+while q pidof i3lock; do
+	sleep 1
+done
+log end rest
+xdark $xd_save
+
+# ----------------------------------------------
+
+# run the script again
+
+log restart
+exec $0
+
+# ----------------------------------------------
