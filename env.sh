814a9317c2511a810e68fabff3c94d102a30045b
Sam Watkins <sam@ucm.dev>
2024-09-22 11:45:26 +1000

    feat(env): add test directories to PATH
    - Include python/tests, bash/tests, and perl/tests in PATH

--- env.sh
+++ env.sh
@@ -47,7 +47,7 @@ for dir in perl; do
 done
 
 # TODO only use canon and alias
-for dir in canon alias adm core sys tools text data image audio video code llm anthropic chat voice-chat eg www html markup i3 git gradio wordpress python perl scrape misc youtube email prompt unprompted geo subs files bash tty ally plan time; do
+for dir in canon alias adm core sys tools text data image audio video code llm anthropic chat voice-chat eg www html markup i3 git gradio wordpress python perl scrape misc youtube email prompt unprompted geo subs files bash tty ally plan time python/tests bash/tests perl/tests; do
 	PATH=$PATH:$ALLEMANDE_HOME/$dir
 done
 
