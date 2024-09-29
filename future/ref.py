0cf15edbbf9a147df8f882e21e6011f702a27dcd
Sam Watkins <sam@ucm.dev>
2024-09-29 12:32:23 +1000

    refactor(ref): move 'ref' module to future

--- future/ref.py
+++ future/ref.py
@@ -1,31 +1,7 @@
-""" This module allows you to create references to variables, object
-attributes, dictionary items and array items, and modify them dynamically. """
-
-
-class Proxy:
-    def __init__(self, obj):
-        self._obj = obj
-
-    def __getattribute__(self, name):
-        if name == '_obj':
-            return object.__getattribute__(self, name)
-        return getattr(object.__getattribute__(self, '_obj'), name)
-
-    def __setattr__(self, name, value):
-        if name == '_obj':
-            object.__setattr__(self, name, value)
-        else:
-            setattr(self._obj, name, value)
-
-    def __delattr__(self, name):
-        delattr(self._obj, name)
-
-    def __dir__(self):
-        return dir(self._obj)
-
-    def __getattr__(self, name):
-        return getattr(self._obj, name)
-
+"""
+This module allows you to create references to variables, object
+attributes, dictionary items, and array items, and modify them dynamically.
+"""
 
 class AttrReference:
     def __init__(self, obj, attr):
@@ -45,7 +21,7 @@ class KeyReference:
         self._ref_key = key
 
     def refget(self):
-        return self._ref_obj[self.key]
+        return self._ref_obj[self._ref_key]
 
     def refset(self, value):
         self._ref_obj[self._ref_key] = value
@@ -53,12 +29,12 @@ class KeyReference:
 
 class AttrReferenceProxy(AttrReference):
     def __getattribute__(self, name):
-        if name in ['_ref_obj', '_ref_attr', 'refget', 'refset']:
+        if name in {'_ref_obj', '_ref_attr', 'refget', 'refset'}:
             return object.__getattribute__(self, name)
         return getattr(self.refget(), name)
 
     def __setattr__(self, name, value):
-        if name in ['_ref_obj', '_ref_attr']:
+        if name in {'_ref_obj', '_ref_attr'}:
             object.__setattr__(self, name, value)
         else:
             setattr(self.refget(), name, value)
@@ -68,19 +44,16 @@ class AttrReferenceProxy(AttrReference):
 
     def __dir__(self):
         return dir(self.refget())
-
-    def __getattr__(self, name):
-        return getattr(self.refget(), name)
 
 
 class KeyReferenceProxy(KeyReference):
     def __getattribute__(self, name):
-        if name in ['_ref_obj', '_ref_key', 'refget', 'refset']:
+        if name in {'_ref_obj', '_ref_key', 'refget', 'refset'}:
             return object.__getattribute__(self, name)
         return getattr(self.refget(), name)
 
     def __setattr__(self, name, value):
-        if name in ['_ref_obj', '_ref_key']:
+        if name in {'_ref_obj', '_ref_key'}:
             object.__setattr__(self, name, value)
         else:
             setattr(self.refget(), name, value)
@@ -91,19 +64,17 @@ class KeyReferenceProxy(KeyReference):
     def __dir__(self):
         return dir(self.refget())
 
-    def __getattr__(self, name):
-        return getattr(self.refget(), name)
-
 
 def ref(obj, item=None, key=None, attr=None):
     if item is not None:
-        if getattr(obj, '__getitem__', None):
+        if hasattr(obj, '__getitem__'):
             key = item
         else:
             attr = item
-    if key is None:
-        return AttrReferenceProxy(obj, attr)
-    elif attr is not None:
+
+    if key is not None and attr is None:
         return KeyReferenceProxy(obj, key)
+    elif attr is not None and key is None:
+        return AttrReferenceProxy(obj, attr)
     else:
-        raise ValueError('Invalid reference, must specify item, key, or attr')
+        raise ValueError('Invalid reference, must specify either key or attr exclusively.')
