29d0bc72efd25a5072738a017366555dfb2a6d72
Sam Watkins <sam@ucm.dev>
2024-10-01 10:27:01 +1000

    feat(io): Add support for line and chunk-based I/O
    - New options for `put()`: lines, chunks, rstrip
    - New options for `get()`: lines, chunks, rstrip
    - Improve handling of line endings in both functions

--- ally/main.py
+++ ally/main.py
@@ -331,7 +331,7 @@ def io(istream: TextIO = sys.stdin, ostream: TextIO = sys.stdout) -> tuple[Calla
     """
     is_tty = tty.is_tty(istream) and tty.is_tty(ostream)
 
-    def put(*args, end="\n", **kwargs) -> None:
+    def put(*args, end="\n", lines=False, chunks=False, rstrip=True, **kwargs) -> None:
         """
         Print to the output stream.
 
@@ -339,22 +339,39 @@ def io(istream: TextIO = sys.stdin, ostream: TextIO = sys.stdout) -> tuple[Calla
             *args: Positional arguments to print.
             **kwargs: Keyword arguments for print function.
         """
+        if lines or chunks:
+            for line in args:
+                if rstrip:
+                    line = line.rstrip()
+                elif not chunks:
+                    line = line.rstrip("\n")
+                print(line, file=ostream, end=end, **kwargs)
+            return
         if args and args[-1].endswith("\n"):
             end = ""
         print(*args, file=ostream, end=end, **kwargs)
 
-    def get(prompt: str = ": ", all=False) -> str:
+    def get(prompt: str = ": ", all=False, lines=False, chunks=False, rstrip=True) -> str:
         """
         Get input from the input stream.
 
         Args:
             prompt (str, optional): Prompt to display. Defaults to ": ".
+            all (bool, optional): Read all input. Defaults to False.
+            lines (bool, optional): Read all lines to a list. Defaults to False.
 
         Returns:
             str: The input string.
         """
         if all:
             return istream.read()
+        if lines or chunks:
+            all_lines = istream.readlines()
+            if lines and rstrip:
+                all_lines = [line.rstrip() for line in all_lines]
+            elif not chunks:
+                all_lines = [line.rstrip("\n") for line in all_lines]
+            return all_lines
         if is_tty:
             tty.setup_history()
             return tty.get(prompt)
