f823ae9f53c7f859126d343479fc7c136ef15c56
Sam Watkins <sam@ucm.dev>
2024-10-01 22:43:06 +1000

    chore(cleanup): alt setup for glxgears.c

---
+++ snip/glxgears.c
@@ -0,0 +1,15 @@
+	static int glx_attribs[] =
+	{
+		GLX_X_RENDERABLE    , True,
+		GLX_DRAWABLE_TYPE   , GLX_WINDOW_BIT,
+		GLX_RENDER_TYPE     , GLX_RGBA_BIT,
+		GLX_X_VISUAL_TYPE   , GLX_TRUE_COLOR,
+		GLX_RED_SIZE        , 8,
+		GLX_GREEN_SIZE      , 8,
+		GLX_BLUE_SIZE       , 8,
+		GLX_ALPHA_SIZE      , 8,
+		GLX_DEPTH_SIZE      , 24,
+		GLX_STENCIL_SIZE    , 8,
+		GLX_DOUBLEBUFFER    , True,
+		None
+	};
