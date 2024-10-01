0aa6caa1dd75fb38b2542ea7ab8f26c47c4fff34
Sam Watkins <sam@ucm.dev>
2024-10-01 18:03:40 +1000

    feat(unrubbish): add script to recover last file from ~/.rubbish
    - Move the most recently changed file from ~/.rubbish to current directory
    - Strip timestamp suffix from filename when restoring

---
+++ files/unrubbish.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# [file]
+# Move the last changed file from ~/.rubbish/ back to CWD
+
+unrubbish() {
+	. opts
+
+	# strict mode
+	local old_opts=$(set +o)
+	set -e -u -o pipefail
+	trap 'eval "$old_opts"' RETURN
+
+	local rubbish_dir=~/.rubbish
+	local last_file
+
+	# Find the last changed file in the rubbish directory
+	last_file=$( ( i "$rubbish_dir/" ls -tc1 || true ) | head -n1)
+
+	if [ -z "$last_file" ]; then
+		echo >&2 "No files found in $rubbish_dir"
+		return 1
+	fi
+
+	# Extract the original filename by removing the timestamp suffix
+	local original_name=$(printf "%s" "$last_file" | sed -E 's/_[0-9]{8}_[0-9]{15}\+[0-9]{4}_[a-z]{3}_[0-9]+$//')
+
+	# Move the file back to the current working directory
+	v mv -i "$rubbish_dir/$last_file" "$original_name"
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	unrubbish "$@"
+fi
