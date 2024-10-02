eabee8a099a59d06f1b32cdc77013870ab260482
Sam Watkins <sam@ucm.dev>
2024-10-03 06:42:20 +1000

    refactor(text): Improve join_files and split_files scripts
    - join_files: Skip CVS directories and handle files without newlines
    - split_files: Preserve separator when splitting files
    - Both scripts: Improve code formatting and readability

--- text/join_files.pl
+++ text/join_files.pl
@@ -11,49 +11,54 @@ my $sep = $ENV{JF_SEP} || '#File:';
 use IO::File;
 
 my $dest = shift;
-if (!defined $dest) {
+if ( !defined $dest ) {
     die "usage: jf all.txt [file1.txt ...]\nsee:   sf\n";
 }
 
 my @src = @ARGV;
 
-if ( -e $dest &&
-	(! -f _ || -s _ &&
-		index(IO::File->new($dest)->
-			getline(), "$sep ") != 0
-	)
-   ) {
-die "would clobber: $dest\n";
+if (
+    -e $dest
+    && (  !-f _
+        || -s _ && index( IO::File->new($dest)->getline(), "$sep " ) != 0 )
+  )
+{
+    die "would clobber: $dest\n";
 }
 
 my $destfh = new IO::File(">$dest")
-    or die "cannot open $dest";
+  or die "cannot open $dest";
 
-while (my $src = shift @src) {
+while ( my $src = shift @src ) {
     chomp $src;
-    if (-T $src) {
-	print "processing textfile $src\n";
-	# we only want to munge text files, not binary
-	my $srcfh = new IO::File($src)
-	    or die "cannot open $src";
-	print $destfh $sep, ' ', $src, "\n";
-	while (my $line = <$srcfh>) {
-	    if (index ($line, $sep) == 0) {
-		substr $line, length($sep), 0, '-'; #insert a - after it to distinguish from a separator, which has a space
-	    }
-	    unless (substr($line, length($line)-1) eq "\n") {
-		# oh no, doesn't end in a newline
-		print "Oh no, doesn't end in a newline!\n";
-		$line.="\n";
-	    }
-	    print $destfh $line;
-	}
-    } elsif (-d $src and $src !~ /CVS$/) {
-	print "reading directory $src\n";
-	opendir DIR, $src;
-	push @src,  map {"$src/$_"} grep {not ($_ eq '.' or $_ eq '..')} readdir DIR;
-    } else {
-	print "ignoring $src\n";
+    if ( -T $src ) {
+        print "processing textfile $src\n";
+
+        # we only want to munge text files, not binary
+        my $srcfh = new IO::File($src)
+          or die "cannot open $src";
+        print $destfh $sep, ' ', $src, "\n";
+        while ( my $line = <$srcfh> ) {
+            if ( index( $line, $sep ) == 0 ) {
+                substr $line, length($sep), 0, '-'; #insert a - after it to distinguish from a separator, which has a space
+            }
+            unless ( substr( $line, length($line) - 1 ) eq "\n" ) {
+
+                # oh no, doesn't end in a newline
+                print "Oh no, doesn't end in a newline!\n";
+                $line .= "\n";
+            }
+            print $destfh $line;
+        }
+    }
+    elsif ( -d $src ) {
+        print "reading directory $src\n";
+        opendir DIR, $src;
+        push @src,
+          map { "$src/$_" } grep { not( $_ eq '.' or $_ eq '..' ) } readdir DIR;
+    }
+    else {
+        print "ignoring $src\n";
     }
 }
 
--- text/split_files.pl
+++ text/split_files.pl
@@ -6,56 +6,59 @@
 use strict;
 
 my $sep = $ENV{JF_SEP} || '#File:';
+
 #my $sep = '!)@(#*$&%^';
 
 use IO::File;
 
 my $src = shift;
-if (!defined $src) {
+if ( !defined $src ) {
     die "usage: sf all.txt\nsee:   jf\n";
 }
 
 my $srcfh = new IO::File("$src")
-	or die "cannot open $src\n";
+  or die "cannot open $src\n";
 
 my $line = <$srcfh>;
-die "file should start with $sep\n" unless index($line, "$sep ") == 0;
+die "file should start with $sep\n" unless index( $line, "$sep " ) == 0;
 
 do {
-	my $dest;
-	chomp ($dest = substr $line, length($sep)+1);
+    my $dest;
+    chomp( $dest = substr $line, length($sep) + 1 );
 
-	rename $dest, "$dest~" if -e $dest;
+    rename $dest, "$dest~" if -e $dest;
 
-	my $i = -1;
-	while (($i = index $dest, '/', $i+1) != -1) {
-		my $dir = substr $dest, 0, $i;
-		unless ($dir eq "" || -d $dir) {
-			mkdir $dir, 0777
-				or die "cannot mkdir $dir\n";
-#			print "making directory $dir\n";
-		}
-	}
+    my $i = -1;
+    while ( ( $i = index $dest, '/', $i + 1 ) != -1 ) {
+        my $dir = substr $dest, 0, $i;
+        unless ( $dir eq "" || -d $dir ) {
+            mkdir $dir, 0777
+              or die "cannot mkdir $dir\n";
 
-	my $destfh = new IO::File(">$dest")
-		or die "cannot open $dest\n";
+            #			print "making directory $dir\n";
+        }
+    }
 
-	while (defined ($line = <$srcfh>)) {
-		if (index($line, $sep) == 0) {
-			last if index($line, "$sep ") == 0;
-			$line = substr $line, length($sep) + 1;
-		}
-		print $destfh $line;
-	}
+    my $destfh = new IO::File(">$dest")
+      or die "cannot open $dest\n";
 
-	close $destfh;
+    while ( defined( $line = <$srcfh> ) ) {
+        if ( index( $line, $sep ) == 0 ) {
+            last if index( $line, "$sep " ) == 0;
+            $line = $sep . substr $line, length($sep) + 1;
+        }
+        print $destfh $line;
+    }
 
-	# undo the change if there was none
-	if (-e "$dest~" && ! system "cmp", "-s", $dest, "$dest~") {
-		print "= $dest\n";
-		rename "$dest~", $dest;
-	} else {
-		print "> $dest\n";
-		system "chmod", "--reference=$dest~", $dest;
-	}
-} while (defined $line);
+    close $destfh;
+
+    # undo the change if there was none
+    if ( -e "$dest~" && !system "cmp", "-s", $dest, "$dest~" ) {
+        print "= $dest\n";
+        rename "$dest~", $dest;
+    }
+    else {
+        print "> $dest\n";
+        system "chmod", "--reference=$dest~", $dest;
+    }
+} while ( defined $line );
