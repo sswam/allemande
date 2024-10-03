fdd8a28078b1689e66086b82f1047ea85177e20a
Sam Watkins <sam@ucm.dev>
2024-10-03 14:51:36 +1000

    feat(web_text): add selenium option for dynamic content
    - Add options for using get-selenium with configurable sleep time
    - Implement conditional get function based on selenium option

--- www/web_text.sh
+++ www/web_text.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -eu
 # wget_text:	get a webpage as simple plain text / markdown
 
+s=	sleep=	# use get-selenium and sleep before dumping
+. opts
+sleep=${sleep:-$s}
+
 url="$1"
 tmp=`mktemp`
-WG_OPTS=-nv wg -O=- "$url" | htmldebloater > "$tmp"
+
+get() {
+	if [ "$s" ]; then
+		get-selenium -s "$s" "$url"
+	else
+		WG_OPTS=-nv wg -O=- "$url"
+	fi
+}
+
+get | htmldebloater > "$tmp"
 pandoc-dump "$tmp" "$url"
 rm "$tmp"
