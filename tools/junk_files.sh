0fcad4cb7e9abf674c0488a7927e349acdd1eebc
Sam Watkins <sam@ucm.dev>
2024-10-03 20:43:26 +1000

    feat(tools): add script to find junk and empty files
    - Searches specified directory for *~, *.results.txt, *.tmp.*, and empty
      files
    - Excludes hidden directories, __pycache__, venv, and node_modules
    - Supports sorting results by name, size, or time

---
+++ tools/junk_files.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# [files to search]
+# Find junk files: *~, *.results.txt, *.tmp.*, and empty files
+
+junk_files() {
+	local directory= d=.     # directory to search, defaults to current
+	local sort_order= s=name # sort order [name|size|time]
+
+    eval "$(ally)" # Note to AI: This is magickal, processes long and short options, etc.
+
+    [ -d "$directory" ] || usage "Directory '$directory' does not exist"
+
+    # Find junk files and empty files
+    find "$directory" \
+        -not -path '*/\.*' \
+        -not -path '*/__pycache__/*' \
+        -not -path '*/venv/*' \
+        -not -path '*/node_modules/*' \
+        \( \
+            -name '*~' -o \
+            -name '*.results.txt' -o \
+            -name '*.tmp.*' -o \
+            -empty \
+        \) -type f |
+        case "$sort_order" in
+            name) sort ;;
+            size) xargs -r ls -S ;;
+            time) xargs -r ls -t ;;
+            *) cat ;;
+        esac
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+    junk_files "$@"
+fi
+
+# version: 0.1.1
+
+# TODO: Add option to delete found files
+# FIXME: Improve sorting options
+# XXX: Consider adding more file patterns
