d165389f74c9a1cef954a7782197a653309eb8b0
Sam Watkins <sam@ucm.dev>
2024-10-06 01:28:34 +1000

    feat(story): add image cycling userscript for markdown files
    - Implement cycling through numbered images on click
    - Set appropriate metadata for the userscript

--- apps/story/story.user.js
+++ apps/story/story.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Cycle Images
 // @namespace    http://ucm.dev/
 // @version      2024-10-04
-// @description  try to take over the world!
-// @author       You
+// @description  Cycle numbered images on click
+// @author       Sam Watkins
 // @match        file://*/*.md
 // @match        *://*/*.md
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.
