54e5c6c06f06b3354213926e56e8262c5d77bfaa
Sam Watkins <sam@ucm.dev>
2024-10-09 04:14:21 +1100

    feat(text): add uniqo script for unique lines in unsorted files
    - Preserves original line order
    - Supports options for blank line handling
    - Reads from files or stdin

---
+++ text/uniqo.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -n
+
+# `uniqo' - like uniq, but it works on unsorted files, and preserves the order of lines
+# Usage: uniqo [-b] [-s] [file...]
+# Options:
+#   -b  include all blank lines
+#   -B  remove all blank lines
+#   -s  squeeze blank lines (implies -b)
+# Example: uniqo -b input.txt > output.txt
+
+BEGIN {
+	use Getopt::Std;
+	getopts('bBs');
+	our $opt_b;
+	our $opt_B;
+	our $opt_s;
+	if ($opt_s) {
+		$opt_b = 1;  # -s implies -b
+	}
+	@ARGV = ();  # Clear @ARGV to allow reading from STDIN if no files specified
+	our $prev_blank = 1;  # Initialize previous line as blank
+}
+
+# Process blank lines if -b option is set
+if ($opt_B && /^$/) {
+} elsif ($opt_b && /^$/) {
+	if (!($opt_s && !$prev_blank)) {
+		print;
+		$prev_blank = 1;
+	}
+} elsif (! exists $already{$_}) {
+	# Process non-blank lines or all lines if -b is not set
+	undef $already{$_};  # Mark line as seen
+	print;  # Print unique line
+	$prev_blank = 0;
+}
