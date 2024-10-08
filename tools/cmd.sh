652dcf1f01c9fa271431c696d2763321c1872455
Sam Watkins <sam@ucm.dev>
2024-10-09 03:24:00 +1100

    feat(tools): cmd.sh default subcommand

--- tools/cmd.sh
+++ tools/cmd.sh
@@ -7,15 +7,15 @@ base=$(basename "$0")
 subdir=$dir/${base%.*}.d
 
 case "${1:-}" in
-""|-h|--help|help)
+-h|--help|help)
 	ls -1 "$subdir"
 	[ -n "${1:-}" ]
 	exit 0
 	;;
 esac
 
-sub=$1
-shift
+sub=${1:-default}
+shift || true
 
 exec "$subdir/$sub" "$@"
 exit 1
