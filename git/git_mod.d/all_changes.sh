bbccd5cbcf0012ff5e1009b82c66054939f464a1
Sam Watkins <sam@ucm.dev>
2024-10-09 04:17:59 +1100

    fix(git): all_changes.sh use new name for renamed files

--- git/git_mod.d/all_changes.sh
+++ git/git_mod.d/all_changes.sh
@@ -1,2 +1,12 @@
-#!/bin/sh
-git status -s | awk '{print $2}'
+#!/usr/bin/env bash
+
+(
+	# Get staged changes
+	git diff --name-status --cached | kutout 1 | sed 's/.*\t//'
+
+	# Get unstaged changes
+	git diff --name-status | kutout 1 | sed 's/.*\t//'
+
+	# Get untracked files
+	git ls-files --others --exclude-standard
+) | uniqo
