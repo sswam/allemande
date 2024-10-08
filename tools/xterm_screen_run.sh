36b8845e2aa3f7b78448bd02d3c3a44900228aa0
Sam Watkins <sam@ucm.dev>
2024-10-09 03:23:25 +1100

    refactor(tools): xterm_screen_run.sh in ally style

--- tools/xterm_screen_run.sh
+++ tools/xterm_screen_run.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
-# xtrem-screen-run.sh: run a command in a new screen window
-screen=$1
-window=$2
-shift 2
-screen-run.sh "$screen" "$window" "$@"
-if [ -n "$DISPLAY" ]; then
-	v xterm -e screen -x "$screen" -p "$window" & disown
+#!/usr/bin/env bash
+
+# screen window command [arg ...]
+# Run a command in a new screen window and optionally open an xterm
+
+xterm-screen-run() {
+	eval "$(ally)"
+
+	local screen=$1
+	local window=$2
+	shift 2
+
+	screen-run "$screen" "$window" "$@" </dev/tty
+	if [ -n "$DISPLAY" ]; then
+		xterm -e screen -x "$screen" -p "$window" & disown
+	fi
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	xterm-screen-run "$@"
 fi
+
+# version: 0.1.1
