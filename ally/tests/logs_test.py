9fe5148d84cc7afaf1831da9da3f7fe1371157bc
Sam Watkins <sam@ucm.dev>
2024-10-12 05:09:28 +1100

    feat(tests): add unit tests for logs, meta, and opts modules
    - Add tests for get_logger, get_log_level, setup_logging, and IndentLogger
    - Include tests for get_module_name and get_script_name functions
    - Add tests for CustomHelpFormatter and setup_logging_args

---
+++ ally/tests/logs_test.py
@@ -0,0 +1,62 @@
+import os
+import io
+import sys
+import pytest
+from unittest.mock import patch, MagicMock, mock_open
+from pathlib import Path
+import tempfile
+import logging
+import argparse
+
+import ally.logs as subject
+
+subject_name = subject.__name__
+
+__version__ = "0.1.4"
+
+
+def test_get_logger():
+    logger = subject.get_logger()
+    assert logger.name == 'logs_test'
+
+
+def test_get_log_level():
+    with patch(f'{subject_name}.get_logger') as mock_get_logger:
+        mock_logger = MagicMock()
+        mock_handler = MagicMock()
+        mock_handler.level = 20  # INFO level
+        mock_logger.handlers = [mock_handler]
+        mock_get_logger.return_value = mock_logger
+
+        assert subject.get_log_level() == 'INFO'
+
+
+@patch('os.makedirs')
+@patch('logging.FileHandler')
+@patch('logging.StreamHandler')
+@patch('os.chmod')
+def test_setup_logging(mock_chmod, mock_stream_handler, mock_file_handler, mock_makedirs):
+    """
+    Test the setup_logging function to ensure it configures logging correctly.
+    """
+    mock_logger = MagicMock()
+    with patch(f'{subject_name}.get_logger', return_value=mock_logger):
+        subject.setup_logging('test_module', 'DEBUG')
+
+    mock_makedirs.assert_called_once_with(os.path.expanduser("~/.logs"), exist_ok=True, mode=0o700)
+    mock_file_handler.assert_called_once()
+    mock_stream_handler.assert_called_once()
+    mock_logger.debug.assert_called_with("Starting test_module")
+    mock_chmod.assert_called_once()
+
+
+def test_IndentLogger():
+    mock_logger = MagicMock()
+    indent_logger = subject.IndentLogger(mock_logger)
+
+    indent_logger.debug("Test message")
+    mock_logger.debug.assert_called_with("Test message")
+
+    indent_logger.indent(1)
+    indent_logger.debug("Indented message")
+    mock_logger.debug.assert_called_with("\tIndented message")
