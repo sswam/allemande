53b1bd4358951d42995bbf3e4c1ef9c18c265dc7
Sam Watkins <sam@ucm.dev>
2024-10-11 11:34:44 +1100

    feat(move-rubbish): Improve script robustness and portability
    - Use /usr/bin/env bash shebang
    - Add usage comment and undo information
    - Implement ally for better environment handling
    - Improve variable expansion and quoting
    - Add support for custom RUBBISH directory

--- files/move_rubbish.sh
+++ files/move_rubbish.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-# mr - move to rubbish
-# suggest alias rm=mr
-# mr @files
-# moves each of @files to ~/rubbish/${basename}_$(nano=1 dt0`
-if [ -z "$RUBBISH" ]; then
-	M=$(mount-point "$1")
+#!/usr/bin/env bash
+# [file ...]
+# move to rubbish
+# suggested: alias rm=move-rubbish
+# moves each file to ~/rubbish/${basename}_$(nano=1 dt0`
+# undo with unrubbish
+
+eval "$(ally)"
+
+if [ -z "${RUBBISH:-}" ]; then
+	M=$(mount-point "${1:-}")
 	if [ "$M" = "$(mount-point "$HOME")" -o "$M" = / ]; then
 		RUBBISH="$HOME/.rubbish"
 	else
@@ -20,6 +24,6 @@ for A; do
 		[ -e "$B" ] || break  # XXX not entirely secure, should use >| to creat or something?
 	done
 	mv -v -i -- "$A" "$B" || status=1
-	[ -n "$mr_echo" ] && echo "$DEST"
+	[ -n "${mr_echo:-}" ] && echo "$DEST"
 done
 exit $status
