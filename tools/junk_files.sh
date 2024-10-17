3a9ca4a7b0d487b0501108cdcbed6fc9208c6c33
Sam Watkins <sam@ucm.dev>
2024-10-18 10:33:52 +1100

    feat(junk_files): add remove option and improve file matching
    - Add remove flag to move found junk files to rubbish
    - Adjust file pattern matching for hidden results files
    - Exclude hidden directories from search

--- tools/junk_files.sh
+++ tools/junk_files.sh
@@ -4,42 +4,48 @@
 # Find junk files: *~, *.results.txt, *.tmp.*, and empty files
 
 junk_files() {
-	local directory= d=.     # directory to search, defaults to current
-	local sort_order= s=name # sort order [name|size|time]
+    local directory= d=.     # directory to search, defaults to current
+    local sort_order= s=name # sort order [name|size|time]
+    local remove= r=0    # remove files flag
 
     eval "$(ally)" # Note to AI: This is magickal, processes long and short options, etc.
 
     [ -d "$directory" ] || usage "Directory '$directory' does not exist"
 
     # Find junk files and empty files
-    find "$directory" \
-        -not -path '*/\.*' \
+    found_files=$(find "$directory" \
+        -not -path '*/\.*/*' \
         -not -path '*/__pycache__/*' \
         -not -path '*/venv/*' \
         -not -path '*/node_modules/*' \
         \( \
             -name '*~' -o \
-            -name '*.results.txt' -o \
+            -name '.*.results.txt' -o \
             -name '*.tmp.*' -o \
             -empty -o \
             -size 1c \
         \) -type f -printf "%P\n" |
     sort |
-    comm -3 -2 - <(git ls-files | sort) |
-    case "$sort_order" in
-        name) sort ;;
-        size) xa -r ls -1 -S ;;
-        time) xa -r ls -1 -t ;;
-        *) cat ;;
-    esac
+    comm -3 -2 - <(git ls-files | sort))
+
+    if (( remove )); then
+        (
+            cd "$directory"
+            printf "%s\n" "$found_files" | xa move-rubbish
+        )
+    else
+        echo "$found_files" |
+        case "$sort_order" in
+            name) sort ;;
+            size) xa ls -1 -S ;;
+            time) xa ls -1 -t ;;
+            *) cat ;;
+        esac
+    fi
 }
 
 if [ "${BASH_SOURCE[0]}" = "$0" ]; then
     junk_files "$@"
 fi
 
-# version: 0.1.1
-
-# TODO: Add option to delete found files
-# FIXME: Improve sorting options
-# XXX: Consider adding more file patterns
+# version: 0.1.2
