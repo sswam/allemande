62833b3c48b4302a20a5507740c839e13c74fe89
Sam Watkins <sam@ucm.dev>
2024-10-26 23:35:33 +1100

    feat(git): improve git-ignore-elf script functionality
    - Add support for verbose output
    - Use bash instead of sh for better compatibility
    - Implement as a function for potential reuse
    - Add version number

--- git/git_ignore_elf.sh
+++ git/git_ignore_elf.sh
@@ -1,12 +1,34 @@
-#!/bin/sh
-set -eu
-cd "$(git-root)"
-file=.gitignore
-new=$file.new.$$
-(
-if [ -e "$file" ]; then
-	cat "$file"
+#!/usr/bin/env bash
+
+# [files...]
+# Updates .gitignore with ELF files in the git repository
+
+git-ignore-elf() {
+	local file=.gitignore
+	local verbose= v=
+
+	eval "$(ally)"
+
+	cd "$(git-root)" || die "Not in a git repository"
+
+	local new="$file.new.$$"
+
+	{
+		if [ -e "$file" ]; then
+			cat "$file"
+		fi
+		find-elf . | grep -v '/\.' | sed 's,^./,/,'
+	} | uniqo >"$new"
+
+	if [ "$verbose" = 1 ]; then
+		printf >&2 "Updating %s\n" "$file"
+	fi
+
+	mv "$new" "$file"
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	git-ignore-elf "$@"
 fi
-find-elf . | grep -v '/\.' | sed 's,^./,/,'
-) | uniqo >"$new"
-mv "$new" "$file"
+
+# version: 0.1.0
