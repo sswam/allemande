3aa954cdf137a7507abf20ef0784e29b884d5f29
Sam Watkins <sam@ucm.dev>
2024-10-26 23:02:52 +1100

    feat(hello_files): improve file search with find command
    - Replace ls with find for more robust file discovery
    - Add maxdepth limit to avoid searching unnecessarily deep

--- files/hello_files.sh
+++ files/hello_files.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
+
 # [arg ...]
 # Show the different hello_foo.foo example files in Allemande
 cd "$ALLEMANDE_HOME"
-ls */hello*.* | perl -ne 'print if m{/hello_(\w+)\.(\1)$}'
+find . -maxdepth 2 -type f -name "hello_*.*" | perl -ne 'print if m{/hello_(\w+)\.(\1)$}'
