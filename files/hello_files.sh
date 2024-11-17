da11b46bdb66a4534b09f402e664d841235dc1f5
Sam Watkins <sam@ucm.dev>
2024-11-18 05:26:04 +1100

    fix(files): Make hello_files.sh list symlinks to hello examples too

--- files/hello_files.sh
+++ files/hello_files.sh
@@ -3,4 +3,4 @@
 # [arg ...]
 # Show the different hello_foo.foo example files in Allemande
 cd "$ALLEMANDE_HOME"
-find . -maxdepth 2 -type f -name "hello_*.*" | perl -ne 'print if m{/hello_(\w+)\.(\1)$}'
+find . -maxdepth 2 -name "hello_*.*" | perl -ne 'print if m{/hello_(\w+)\.(\1)$}'
