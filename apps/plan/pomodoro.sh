b6fd0c15dca04da8a426252d31b01c43f22eb557
Sam Watkins <sam@ucm.dev>
2024-11-18 09:33:13 +1100

    feat(pomodoro): restore standard pomodoro timings (25/3 min)
    - Changed debug timings back to standard 25 min work / 3 min rest periods
    - Restored 1 min grace/fade periods

--- apps/plan/pomodoro.sh
+++ apps/plan/pomodoro.sh
@@ -3,14 +3,14 @@
 # pomodoro: Pomodoro timer
 # Version: 0.2.1
 
-# grace=$[1*60]      # Set the grace time (in seconds)
-# fade=$[1*60]       # Set the fade time, or 0 to 'annoy' (in seconds)
-# work=$[25*60]      # Set the working time (in seconds)
-# rest=$[3*60]       # Set the rest time (in seconds)
-grace=50             # Set the grace time (in seconds)
-fade=10              # Set the fade time, or 0 to 'annoy' (in seconds)
-work=$[8*60]         # Set the working time (in seconds)
-rest=$[1*60]         # Set the rest time (in seconds)
+grace=$[1*60]      # Set the grace time (in seconds)
+fade=$[1*60]       # Set the fade time, or 0 to 'annoy' (in seconds)
+work=$[25*60]      # Set the working time (in seconds)
+rest=$[3*60]       # Set the rest time (in seconds)
+# grace=50             # Set the grace time (in seconds)
+# fade=10              # Set the fade time, or 0 to 'annoy' (in seconds)
+# work=$[8*60]         # Set the working time (in seconds)
+# rest=$[1*60]         # Set the rest time (in seconds)
 work_min_percent=80  # Minimum work percent, to align with the clock
 g=	# More grace (or start with rest / grace period)
 q=	# Quit any running pomodoro
