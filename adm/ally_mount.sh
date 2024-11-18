e95d86d3711385a72fca5698a4ff39d7cb440939
Sam Watkins <sam@ucm.dev>
2024-11-19 03:10:04 +1100

    feat(ally-mount): Mount llm_llama portal
    - Extract shared portal mount/umount code into functions
    - Remove sudo -u allemande calls which were causing permission problems

--- adm/ally_mount.sh
+++ adm/ally_mount.sh
@@ -8,16 +8,29 @@ umount=$u
 ally-mount() {
 	mkdir -p $ALLEMANDE_ROOMS_SERVER
 	sshfs -o cache=no -o allow_root -o allow_other -o idmap=none $SERVER_ROOMS_SSH $ALLEMANDE_ROOMS_SERVER || true
-	sudo -u allemande qe rmdir /var/spool/allemande/stt_whisper/www-data/* || true
-	sudo -u allemande sshfs -o cache=no -o allow_root -o allow_other -o idmap=none ucm.dev:/var/spool/allemande/stt_whisper/www-data /var/spool/allemande/stt_whisper/www-data -o cache=no || true
+
+	portal-mount stt_whisper/www-data
+	portal-mount llm_llama/$USER
 }
 
 ally-umount() {
 	fusermount -u $ALLEMANDE_ROOMS_SERVER || true
-	sudo -u allemande fusermount -u /var/spool/allemande/stt_whisper/www-data || true
-	sudo -u allemande qe rmdir /var/spool/allemande/stt_whisper/www-data/* || true
+
+	portal-umount stt_whisper/www-data
+	portal-umount llm_llama/$USER
 }
 
+portal-mount() {
+	local portal=$1
+	qe rmdir /var/spool/allemande/$portal/* || true
+	sshfs -o cache=no -o allow_root -o allow_other -o idmap=none ucm.dev:/var/spool/allemande/$portal /var/spool/allemande/$portal -o cache=no || true
+}
+
+portal-umount() {
+	local portal=$1
+	fusermount -u /var/spool/allemande/$portal || true
+	qe rmdir /var/spool/allemande/$portal/* || true
+}
 
 if [ "$0" = "$BASH_SOURCE" ]; then
 	if [ "$umount" = 1 ]; then
