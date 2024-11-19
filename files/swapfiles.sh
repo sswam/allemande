f795d4be746b2a9d02c856c0dc2c5b69a637a4d5
Sam Watkins <sam@ucm.dev>
2024-11-19 22:56:35 +1100

    feat(swapfiles): add input validation and improve arg names
    - Sensible variable names replace positional params
    - Check file existence and argument count before proceeding

--- files/swapfiles.sh
+++ files/swapfiles.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 # file1 file2
+# Swap the names of two files
 
 copy= c=	# copy to preserve hard links
 
 eval "$(ally)"
 
+if [ "$#" -ne 2 ]; then
+	echo "swapfiles: wrong number of arguments" >&2
+	exit 1
+fi
+
+from="$1"
+to="$2"
+
+if [ ! -e "$from" ] || [ ! -e "$to" ]; then
+	echo "swapfiles: files do not both exist" >&2
+	exit 1
+fi
+
 if [ "$copy" = 1 ]; then
-	cp -i "$1" "$1.tmp.$$"
-	cp "$2" "$1"
-	cp "$1.tmp.$$" "$2"
-	rm "$1.tmp.$$"
+	cp -i "$from" "$from.tmp.$$"
+	cp "$to" "$from"
+	cp "$from.tmp.$$" "$to"
+	rm "$from.tmp.$$"
 else
-	mv -i "$1" "$1.tmp.$$"
-	mv -i "$2" "$1"
-	mv -i "$1.tmp.$$" "$2"
+	mv -i "$from" "$from.tmp.$$"
+	mv -i "$to" "$from"
+	mv -i "$from.tmp.$$" "$to"
 fi
