1607b12409a96b90fc365cda49edcd605d7845fe
Sam Watkins <sam@ucm.dev>
2024-11-21 15:09:29 +1100

    feat(disk-free): script to format df output nicely

---
+++ files/disk-free-human.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+df -h "$@" 2>/dev/null |
+grep -E '^Filesystem|/$|/home$| /media/| /mnt|/dev/mapper' | grep -v '^overlay ' |
+txt2tsv | tsv-tidy | tsv2txt -t |
+kutleft 6 4 5 3 2 | headless order | tsv2txt -t
