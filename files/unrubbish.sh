2dd664903f13a0e3c41ff99c6ee745d2734b98c8
Sam Watkins <sam@ucm.dev>
2024-11-21 15:12:05 +1100

    feat(unrubbish): use rubbish bins for mount point, not just under home
    - Auto-detect the matching mount point for input file or use $HOME/.rubbish

--- files/unrubbish.sh
+++ files/unrubbish.sh
@@ -11,14 +11,22 @@ unrubbish() {
 	set -e -u -o pipefail
 	trap 'eval "$old_opts"' RETURN
 
-	local rubbish_dir=~/.rubbish
+	if [ -z "${RUBBISH:-}" ]; then
+		M=$(mount-point "${1:-}")
+		if [ "$M" = "$(mount-point "$HOME")" -o "$M" = / ]; then
+			RUBBISH="$HOME/.rubbish"
+		else
+			RUBBISH="$M/.rubbish"
+		fi
+	fi
+
 	local last_file
 
 	# Find the last changed file in the rubbish directory
-	last_file=$( ( i "$rubbish_dir/" ls -tc1 || true ) | head -n1)
+	last_file=$( ( i "$RUBBISH/" ls -tc1 || true ) | head -n1)
 
 	if [ -z "$last_file" ]; then
-		echo >&2 "No files found in $rubbish_dir"
+		echo >&2 "No files found in $RUBBISH"
 		return 1
 	fi
 
@@ -26,7 +34,7 @@ unrubbish() {
 	local original_name=$(printf "%s" "$last_file" | sed -E 's/_[0-9]{8}_[0-9]{15}\+[0-9]{4}_[A-Za-z]{3}_[0-9]+$//')
 
 	# Move the file back to the current working directory
-	v mv -i "$rubbish_dir/$last_file" "$original_name"
+	v mv -i "$RUBBISH/$last_file" "$original_name"
 }
 
 if [ "${BASH_SOURCE[0]}" = "$0" ]; then
