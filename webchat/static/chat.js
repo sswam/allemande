7f387ec9b1c81f99c6dc9785ded586ff07465f10
Sam Watkins <sam@ucm.dev>
2024-11-24 09:38:07 +1100

    fix(webchat): disable PWA/service worker functionality temporarily

--- webchat/static/chat.js
+++ webchat/static/chat.js
@@ -329,7 +329,7 @@ async function register_service_worker() {
 	if (!'serviceWorker' in navigator)
 		return;
 	try {
-		const registration = await navigator.serviceWorker.register('/service-worker.js');
+		const registration = await navigator.serviceWorker.register('/service_worker.js');
 		console.log('ServiceWorker registration successful');
 	} catch (err) {
 		console.error('ServiceWorker registration failed: ', err);
@@ -387,7 +387,7 @@ function main() {
 //	$on($id('logout'), 'click', logoutChat);
 	$on($id('notify'), 'click', notify_clicked);
 	authChat();
-	register_service_worker();
+//	register_service_worker();
 }
 
 main();
