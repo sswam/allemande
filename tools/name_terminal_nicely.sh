237bd33e3738aa14a4fe762ea698daa1a363826c
Sam Watkins <sam@ucm.dev>
2024-12-01 16:46:54 +1100

    feat(tools): add custom title support to name_terminal_nicely.sh

--- tools/name_terminal_nicely.sh
+++ tools/name_terminal_nicely.sh
@@ -7,6 +7,9 @@ fi
 if [ -n "$STY" ]; then
     title+="${STY#*.}: "
 fi
+if [ -n "$TERMINAL_TITLE" ]; then
+    title+="$TERMINAL_TITLE: "
+fi
 title+="$*"
 title=${title%: }
 name-terminal "$title"
