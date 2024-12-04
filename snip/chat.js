c1c96e90607793a457c7f00481633068da2bcfd8
Sam Watkins <sam@ucm.dev>
2024-12-05 08:26:53 +1100

    refactor(webchat): simplify file upload response processing
    - Move media markdown generation to server side

---
+++ snip/chat.js
@@ -0,0 +1,27 @@
+
+function stem_ext(filename) {
+	const match = filename.match(/^(.+?)(?:\.([^.]+))?$/);
+	return [match[1], match[2] || ''];
+}
+
+function av_element_html(tag, stem, url) {
+	const $el = $create(tag);
+	$el.ariaLabel = stem;
+	$el.src = url;
+	$el.controls = true;
+	return $el.outerHTML;
+}
+
+	if (medium === 'image') {
+		// insert the image into the message box as markdown
+		$content.value += `![${stem}](${url})`;
+	} else if (medium === 'audio') {
+		// insert the audio into the message box as an audio element
+		$content.value += av_element_html("audio", stem, url);
+	} else if (medium === 'video') {
+		// insert the video into the message box as a video element
+		$content.value += av_element_html("video", stem, url);
+	} else {
+		// insert the URL into the message box as a markdown link
+		$content.value += `[${stem}](${url})`;
+	}
--- webchat/static/chat.js
+++ webchat/static/chat.js
@@ -532,19 +532,6 @@ function files_changed(ev) {
 	ev.target.value = '';
 }
 
-function stem_ext(filename) {
-	const match = filename.match(/^(.+?)(?:\.([^.]+))?$/);
-	return [match[1], match[2] || ''];
-}
-
-function av_element_html(tag, stem, url) {
-	const $el = $create(tag);
-	$el.ariaLabel = stem;
-	$el.src = url;
-	$el.controls = true;
-	return $el.outerHTML;
-}
-
 async function upload_file(file) {
 	// upload in the background using fetch
 	console.log("upload_file", file);
@@ -567,28 +554,14 @@ async function upload_file(file) {
 	const data = await response.json();
 	console.log("upload_file response", data);
 
-	const { name, url, medium } = data;
-
-	const [stem, ext] = stem_ext(name);
+	const { name, url, medium, markdown } = data;
 
 	// make sure the message content ends with whitespace
 	if (!/\s$/.test($content.value)) {
 		$content.value += "\n";  // TODO this kills undo
 	}
 
-	if (medium === 'image') {
-		// insert the image into the message box as markdown
-		$content.value += `![${stem}](${url})`;
-	} else if (medium === 'audio') {
-		// insert the audio into the message box as an audio element
-		$content.value += av_element_html("audio", stem, url);
-	} else if (medium === 'video') {
-		// insert the video into the message box as a video element
-		$content.value += av_element_html("video", stem, url);
-	} else {
-		// insert the URL into the message box as a markdown link
-		$content.value += `[${stem}](${url})`;
-	}
+	$content.value += markdown;
 }
 
 // main ----------------------------------------------------------------------
