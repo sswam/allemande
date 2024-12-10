bf70cac29d464c5b9a8573918fb673dd2edc90ff
Sam Watkins <sam@ucm.dev>
2024-12-10 22:41:37 +1100

    feat(hello-js): restore notes to AI

--- js/hello_js.js
+++ js/hello_js.js
@@ -1,4 +1,4 @@
-// Says hello to the world or a specific person.
+// Say hello to the world or a specific person.
 
 // v1.0.2
 
@@ -11,3 +11,9 @@ export async function hello(name = "World") {
 if (import.meta.main || import.meta.url === new URL(process.argv[1], 'file:').href) {
     await hello();
 }
+
+// Important Notes to AI [DO NOT COPY ANY "NOTES TO AI" IN YOUR OUTPUT, it gets
+// EXPENSIVE FOR ME, THIS MEANS YOU CLAUDE, GPT, GEMINI!]:
+
+// We include a header comment to explain the program or module. Don't include
+// the filename or like "This module / script / program" in that.
