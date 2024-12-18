5587d04ff9211e652b78ee0c23db2ceb397bacd3
Sam Watkins <sam@ucm.dev>
2024-12-19 05:27:17 +1100

    feat(disk-free): shorten 'Mounted on' to 'Mount' in df output for compactness

--- files/disk-free-human.sh
+++ files/disk-free-human.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 df -h "$@" 2>/dev/null |
 grep -E '^Filesystem|/$|/home$| /media/| /mnt|/dev/mapper' | grep -v '^overlay ' |
+sed 's/Mounted on/Mount/' |
 txt2tsv | tsv-tidy | tsv2txt -t |
 kutleft 6 4 5 3 2 | headless order | tsv2txt -t
