66a6333903926065c6d9e46e25528a1abd2a2438
Sam Watkins <sam@ucm.dev>
2025-01-02 14:46:16 +1100

    feat(chat): switch brain.sh to default LLM config instead of experimental

--- chat/brain.sh
+++ chat/brain.sh
@@ -5,7 +5,7 @@
 . unset-option-vars
 OPTS_ALLOW_UNKNOWN=1 eval "$(ally)"
 
-v ally-chat -v -w "$WATCH_LOG" -m "$LLM_MODEL" -c "$ALLEMANDE_HOME/config/llm_llama/experiment.yaml" "$@"
+v ally-chat -v -w "$WATCH_LOG" -m "$LLM_MODEL" -c "$ALLEMANDE_HOME/config/llm_llama/default.yaml" "$@"
 
 # not using options: -r --no-trim --get-roles-from-history
 # -u "$user" -b "$bot" 
