c7acb67905af0fea857d5a9286a791e7d1ae39fe
Sam Watkins <sam@ucm.dev>
2025-01-02 14:46:12 +1100

    feat(monitor): add verbose mode and more precise memory threshold
    - Add -v flag for verbose logging of all checks
    - Bump memory threshold from 80% to 95%
    - Prefix alerts with WARNING
    - Add INFO: prefix for verbose messages

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -20,8 +20,9 @@
 monitor() {
 	local threshold_storage=  s=95       # storage usage threshold percentage
 	local threshold_load=     l=1        # load average threshold
-	local threshold_mem=      m=80       # memory usage threshold percentage
+	local threshold_mem=      m=95       # memory usage threshold percentage
 	local ping_host=          p=8.8.8.8  # host to ping test
+	local verbose=            v=         # verbose
 
 	eval "$(ally)"
 
@@ -34,8 +35,11 @@ monitor() {
 check_disk() {
 	df -h | grep -vE '^Filesystem|tmpfs|udev' | awk '{ print $5 " " $6 }' | while read -r percent mountpoint; do
 		usage=${percent%%%}
+		if [ "$verbose" ]; then
+			printf "INFO: Filesystem mounted at %s is %s%% full\n" "$mountpoint" "$usage"
+		fi
 		if [ "$usage" -ge "$threshold_storage" ]; then
-			printf "Filesystem mounted at %s is %s%% full!\n" "$mountpoint" "$usage"
+			printf "WARNING: Filesystem mounted at %s is %s%% full!\n" "$mountpoint" "$usage"
 		fi
 	done
 }
@@ -45,8 +49,11 @@ check_load() {
 	load_5min=$(uptime | awk -F'load average:' '{ print $2 }' | cut -d, -f2)
 	load_5min=${load_5min// /}
 
+	if [ "$verbose" ]; then
+		printf "INFO: System load is %s (5-minute average)\n" "$load_5min"
+	fi
 	if (($(echo "$load_5min >= $threshold_load" | bc -l))); then
-		printf "System load is high: %s (5-minute average)\n" "$load_5min"
+		printf "WARNING: System load is high: %s (5-minute average)\n" "$load_5min"
 	fi
 }
 
@@ -55,14 +62,27 @@ check_memory() {
 	memory_usage=$(free | grep Mem | awk '{print ($2-$7)/$2 * 100.0}')
 	memory_usage=${memory_usage%.*}
 
+	if [ "$verbose" ]; then
+		printf "INFO: Memory usage is at %s%%\n" "$memory_usage"
+	fi
 	if [ "$memory_usage" -ge "$threshold_mem" ]; then
-		printf "Memory usage is at %s%%!\n" "$memory_usage"
+		printf "WARNING: Memory usage is at %s%%!\n" "$memory_usage"
 	fi
 }
 
 check_ping() {
-	if ! ping -W 1 -c 1 "$ping_host" &>/dev/null; then
-		printf "Server cannot ping %s - possible network issues!\n" "$ping_host"
+	local pink_fail=0
+	ping -W 1 -c 1 "$ping_host" &>/dev/null
+	ping_fail=$?
+	if [ "$verbose" ]; then
+		if [ "$ping_fail" -eq 0 ]; then
+			printf "INFO: Server can ping %s\n" "$ping_host"
+		else
+			printf "INFO: Server cannot ping %s\n" "$ping_host"
+		fi
+	fi
+	if [ "$ping_fail" -ne 0 ]; then
+		printf "WARNING: Server cannot ping %s - possible network issues!\n" "$ping_host"
 	fi
 }
 
