fae13690fd44d079e072c577675be1d58bdcde18
Sam Watkins <sam@ucm.dev>
2025-02-14 17:10:33 +0000

    feat(stt_whisper): calculate confidence per segment and add confidence threshold
    - Added total confidence based on no_speech_prob from segments
    - Added confident flag using configurable threshold

--- audio/mike.py
+++ audio/mike.py
@@ -29,118 +29,118 @@ opts = None
 
 logger = logging.getLogger(__name__)
 
-server = "stt_whisper"
-default_portal = portals.get_default_portal(server)
+service = "stt_whisper"
+default_portal = portals.get_default_portal(service)
 
 
 @contextmanager
 def open_microphone_spamfree(*args, **kwargs):
-	""" Open microphone without spamming stderr """
-	try:
-		with ucm.redirect_stderr_to_dev_null():
-			source = sr.Microphone(*args, **kwargs).__enter__()  # pylint: disable=unnecessary-dunder-call
-		yield source
-	finally:
-		source.__exit__(None, None, None)
+    """ Open microphone without spamming stderr """
+    try:
+        with ucm.redirect_stderr_to_dev_null():
+            source = sr.Microphone(*args, **kwargs).__enter__()  # pylint: disable=unnecessary-dunder-call
+        yield source
+    finally:
+        source.__exit__(None, None, None)
 
 def record_speech(run_event, q_audio, energy, pause, non_speaking_duration, dynamic_energy, device_index, adjust_for_ambient_noise=False):
-	""" Record audio from microphone and put it in the queue """
-	r = sr.Recognizer()
-	r.energy_threshold = energy
-	r.pause_threshold = pause
-	r.non_speaking_duration = non_speaking_duration
-	r.dynamic_energy_threshold = dynamic_energy
-	i = 0
+    """ Record audio from microphone and put it in the queue """
+    r = sr.Recognizer()
+    r.energy_threshold = energy
+    r.pause_threshold = pause
+    r.non_speaking_duration = non_speaking_duration
+    r.dynamic_energy_threshold = dynamic_energy
+    i = 0
 
-	if opts.log_level <= logging.DEBUG:
-		open_microphone = sr.Microphone
-	else:
-		open_microphone = open_microphone_spamfree
+    if opts.log_level <= logging.DEBUG:
+        open_microphone = sr.Microphone
+    else:
+        open_microphone = open_microphone_spamfree
 
-	first = True
+    first = True
 
-	with open_microphone(sample_rate=16000, device_index=device_index) as source:
-		while run_event.is_set():
-			if adjust_for_ambient_noise:
-				logger.debug("Adjusting for ambient noise")
-				r.adjust_for_ambient_noise(source)
+    with open_microphone(sample_rate=16000, device_index=device_index) as source:
+        while run_event.is_set():
+            if adjust_for_ambient_noise:
+                logger.debug("Adjusting for ambient noise")
+                r.adjust_for_ambient_noise(source)
 
-			if first:
-				logger.info("Listening")
-				first = False
-			else:
-				logger.debug("Listening")
-			audio = r.listen(source)
-			logger.debug("Got audio")
+            if first:
+                logger.info("Listening")
+                first = False
+            else:
+                logger.debug("Listening")
+            audio = r.listen(source)
+            logger.debug("Got audio")
 
-			q_audio.put_nowait(audio)
-			i += 1
-	q_audio.put_nowait(None)
+            q_audio.put_nowait(audio)
+            i += 1
+    q_audio.put_nowait(None)
 
 
 def client_request(portal, audio, config=None):
-	""" Call the core server and get a response. """
+    """ Call the core server and get a response. """
 
-	req = portals.prepare_request(portal, config=config)
+    req = portals.prepare_request(portal, config=config)
 
-	req_audio = req/"request.aud"
+    req_audio = req/"request.aud"
 
-	data = io.BytesIO(audio.get_wav_data())
-	audio_clip = AudioSegment.from_file(data)
-	audio_clip.export(str(req_audio), format="flac")
+    data = io.BytesIO(audio.get_wav_data())
+    audio_clip = AudioSegment.from_file(data)
+    audio_clip.export(str(req_audio), format="flac")
 
-	portals.send_request(portal, req)
+    portals.send_request(portal, req)
 
-	resp, status = portals.wait_for_response(portal, req)
+    resp, status = portals.wait_for_response(portal, req)
 
-	if status == "error":
-		portals.response_error(resp)
+    if status == "error":
+        portals.response_error(resp)
 
-	text = (resp/"text.txt").read_text(encoding="utf-8")
-	result = yaml.safe_load((resp/"result.yaml").read_text(encoding="utf-8"))
+    text = (resp/"text.txt").read_text(encoding="utf-8")
+    result = yaml.safe_load((resp/"result.yaml").read_text(encoding="utf-8"))
 
-	logger.info("%r", result)
+    logger.info("%r", result)
 
-	portals.remove_response(portal, resp)
+    portals.remove_response(portal, resp)
 
-	return text, result
+    return text, result
 
 
 def speech_to_text(portal, run_event, q_audio, q_text, lang, confidence_threshold=0.8):
-	""" Transcribe from the audio queue to the text queue """
+    """ Transcribe from the audio queue to the text queue """
 
-	config = {
-		"language": lang,
-	}
+    config = {
+        "language": lang,
+    }
 
-	while run_event.is_set():
-		audio = q_audio.get()
+    while run_event.is_set():
+        audio = q_audio.get()
 
-		if audio is None:
-			break
+        if audio is None:
+            break
 
-#		np_audio = np.frombuffer(audio.get_raw_data(), np.int16)
-#		np_audio = np_audio.flatten().astype(np.float32) / 32768.0
-#		torch_audio = torch.from_numpy(np_audio)
+#        np_audio = np.frombuffer(audio.get_raw_data(), np.int16)
+#        np_audio = np_audio.flatten().astype(np.float32) / 32768.0
+#        torch_audio = torch.from_numpy(np_audio)
 
-		text, result = client_request(portal, audio, config=config)
+        text, result = client_request(portal, audio, config=config)
 
-#		result = whisp.transcribe(torch_audio, language=lang)
+#        result = whisp.transcribe(torch_audio, language=lang)
 
-		logger.info(result)
-		text = text.strip()
-		segs = result["segments"]
-		no_speech_prob = sum(x["no_speech_prob"] for x in segs) / (len(segs) or 1)
-		if text and no_speech_prob < (1 - confidence_threshold):
-			q_text.put_nowait(text)
+        logger.info(result)
+        text = text.strip()
+        segs = result["segments"]
+        no_speech_prob = sum(x["no_speech_prob"] for x in segs) / (len(segs) or 1)
+        if text and no_speech_prob < (1 - confidence_threshold):
+            q_text.put_nowait(text)
 
-	q_text.put_nowait(None)
+    q_text.put_nowait(None)
 
 
 def do_list_devices():
-	""" List available microphone devices """
-	for index, name in enumerate(sr.Microphone.list_microphone_names()):
-		print(f'{index}\t{name}')
+    """ List available microphone devices """
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print(f'{index}\t{name}')
 
 
 @argh.arg("--lang", "-l", default="en", help="Language")
@@ -155,61 +155,61 @@ def do_list_devices():
 @argh.arg("--confidence-threshold", "-c", default=0.6, help="Confidence threshold")
 @argh.arg("--quiet-while-listening", "-m", default=40, help="Quiet or mute speakers while listening, volume level")
 def mike(lang="en", energy=700, dynamic_energy=False, pause=1, non_speaking_duration=1, device_index=None, list_devices=False, adjust_for_ambient_noise=False, portal=default_portal, confidence_threshold=0.6, quiet_while_listening=40):
-	""" Transcribe speech to text using microphone input """
+    """ Transcribe speech to text using microphone input """
 
-	if quiet_while_listening is not None:
-		# TODO put this in a lib, generic and audio control
-		# TODO process / thread safety?
-		# get the current volume
-		vol = alsaaudio.Mixer().getvolume()[0]
-		# set the volume to quiet
-		alsaaudio.Mixer().setvolume(quiet_while_listening)
+    if quiet_while_listening is not None:
+        # TODO put this in a lib, generic and audio control
+        # TODO process / thread safety?
+        # get the current volume
+        vol = alsaaudio.Mixer().getvolume()[0]
+        # set the volume to quiet
+        alsaaudio.Mixer().setvolume(quiet_while_listening)
 
-		# set a handler to reset the volume
-		def reset_volume(*args, exit=True):
-			alsaaudio.Mixer().setvolume(vol)
-			if exit:
-				sys.exit(0)
+        # set a handler to reset the volume
+        def reset_volume(*args, exit=True):
+            alsaaudio.Mixer().setvolume(vol)
+            if exit:
+                sys.exit(0)
 
-		# signal handlers
-		signal.signal(signal.SIGINT, reset_volume)
-		signal.signal(signal.SIGTERM, reset_volume)
+        # signal handlers
+        signal.signal(signal.SIGINT, reset_volume)
+        signal.signal(signal.SIGTERM, reset_volume)
 
-		# on exit
-		atexit.register(reset_volume, exit=False)
+        # on exit
+        atexit.register(reset_volume, exit=False)
 
-	if list_devices:
-		do_list_devices()
-		sys.exit(0)
+    if list_devices:
+        do_list_devices()
+        sys.exit(0)
 
-	portal = Path(portal)
+    portal = Path(portal)
 
-	run_event = Event()
-	run_event.set()
+    run_event = Event()
+    run_event.set()
 
-	try:
-		q_audio = Queue()
-		q_text = Queue()
-		Thread(
-			target=record_speech,
-			args=(run_event, q_audio, energy, pause, non_speaking_duration, dynamic_energy, device_index, adjust_for_ambient_noise)
-			).start()
-		Thread(
-			target=speech_to_text,
-			args=(portal, run_event, q_audio, q_text, lang, confidence_threshold)
-			).start()
-		while True:
-			text = q_text.get()
-			if text is None:
-				break
-			print(text)
-			sys.stdout.flush()
-	except KeyboardInterrupt:
-		logger.exception("Interrupted!")
-	finally:
-		run_event.clear()
+    try:
+        q_audio = Queue()
+        q_text = Queue()
+        Thread(
+            target=record_speech,
+            args=(run_event, q_audio, energy, pause, non_speaking_duration, dynamic_energy, device_index, adjust_for_ambient_noise)
+            ).start()
+        Thread(
+            target=speech_to_text,
+            args=(portal, run_event, q_audio, q_text, lang, confidence_threshold)
+            ).start()
+        while True:
+            text = q_text.get()
+            if text is None:
+                break
+            print(text)
+            sys.stdout.flush()
+    except KeyboardInterrupt:
+        logger.exception("Interrupted!")
+    finally:
+        run_event.clear()
 
 # TODO factor out this main biolerplate stuff
 
 if __name__ == '__main__':
-	ucm_main.run(mike, globals())
+    ucm_main.run(mike, globals())
--- core/stt_whisper.py
+++ core/stt_whisper.py
@@ -29,128 +29,137 @@ prog = main.prog_info()
 ports_dir = Path(os.environ["ALLEMANDE_PORTALS"])/prog.name
 
 def gen(config, audio_file, *_args, model=None, **_kwargs):
-	""" Transcribe text from an audio file. """
+    """ Transcribe text from an audio file. """
 
-	language = config.get("language", "en")
+    language = config.get("language", "en")
 
-#	np_audio = np.frombuffer(audio.get_raw_data(), np.int16)
-#	np_audio = np_audio.flatten().astype(np.float32) / 32768.0
-#	torch_audio = torch.from_numpy(np_audio)
+#    np_audio = np.frombuffer(audio.get_raw_data(), np.int16)
+#    np_audio = np_audio.flatten().astype(np.float32) / 32768.0
+#    torch_audio = torch.from_numpy(np_audio)
 
-	result = model.transcribe(str(audio_file), language=language)
+    result = model.transcribe(str(audio_file), language=language)
 
-	response = {
-		"text.txt": result["text"],
-		"result.yaml": yaml.safe_dump(result),
-	}
+    text = text.strip()
 
-	return response
+
+    # check confidence level
+    segs = result["segments"]
+    no_speech_prob = sum(x["no_speech_prob"] for x in segs) / (len(segs) or 1)
+    result["confidence"] = 1 - no_speech_prob
+    result["confident"] = result["confidence"] >= config.get("confidence_threshold", 0.8)
+
+    response = {
+        "text.txt": result["text"],
+        "result.yaml": yaml.safe_dump(result),
+    }
+
+    return response
 
 
 def load(ports, d, filename):
-	""" Load a file from a directory or above """
-	while True:
-		f = d/filename
-		if f.exists():
-			return f.read_text(encoding="utf-8")
-		if d == ports:
-			break
-		p = d.parent
-		if p == d:
-			break
-		d = p
-	f = prog.dir/filename
-	if f.exists():
-		return f.read_text(encoding="utf-8")
-	raise FileNotFoundError(f"load: could not find {filename} in {d} or above")
+    """ Load a file from a directory or above """
+    while True:
+        f = d/filename
+        if f.exists():
+            return f.read_text(encoding="utf-8")
+        if d == ports:
+            break
+        p = d.parent
+        if p == d:
+            break
+        d = p
+    f = prog.dir/filename
+    if f.exists():
+        return f.read_text(encoding="utf-8")
+    raise FileNotFoundError(f"load: could not find {filename} in {d} or above")
 
 
 def process_request(ports, port, req, fn, *args, **kwargs):
-	""" Process a request on a port """
-	port = Path(port)
-	logger.info("%s:%s - processing", port, req)
-	log_handler = None
-	try:
-		d = port/"doing"/req
-		os.rename(port/"todo"/req, d)
-		log_handler = logging.FileHandler(d/"log.txt")
-		logger.addHandler(log_handler)
+    """ Process a request on a port """
+    port = Path(port)
+    logger.info("%s:%s - processing", port, req)
+    log_handler = None
+    try:
+        d = port/"doing"/req
+        os.rename(port/"todo"/req, d)
+        log_handler = logging.FileHandler(d/"log.txt")
+        logger.addHandler(log_handler)
 
-		config = yaml.safe_load(load(ports, d, "config.yaml"))
-		request = d/"request.aud"
-		response = fn(config, request, *args, **kwargs)
-		for k, v in response.items():
-			(d/k).write_text(v, encoding="utf-8")
-		os.rename(d, port/"done"/req)
-		logger.info("%s:%s - done", port, req)
-	except Exception as e:
-		logger.exception("%s:%s - error: %s", port, req, e)
-		try:
-			os.rename(d, port/"error"/req)
-		except Exception as e2:
-			logger.exception("%s:%s - error: %s", port, req, e2)
-		if 'CUDA error' in str(e):
-			raise
-	finally:
-		if log_handler:
-			logger.removeHandler(log_handler)
+        config = yaml.safe_load(load(ports, d, "config.yaml"))
+        request = d/"request.aud"
+        response = fn(config, request, *args, **kwargs)
+        for k, v in response.items():
+            (d/k).write_text(v, encoding="utf-8")
+        os.rename(d, port/"done"/req)
+        logger.info("%s:%s - done", port, req)
+    except Exception as e:
+        logger.exception("%s:%s - error: %s", port, req, e)
+        try:
+            os.rename(d, port/"error"/req)
+        except Exception as e2:
+            logger.exception("%s:%s - error: %s", port, req, e2)
+        if 'CUDA error' in str(e):
+            raise
+    finally:
+        if log_handler:
+            logger.removeHandler(log_handler)
 
 
 #def port_setup(port):
-#	""" Set up a port """
-#	for box in ("prep", "todo", "doing", "done", "error", "history"):
-#		(port/box).mkdir(exist_ok=True)
+#    """ Set up a port """
+#    for box in ("prep", "todo", "doing", "done", "error", "history"):
+#        (port/box).mkdir(exist_ok=True)
 
 
 def serve_requests(ports, fn):
-	""" Serve requests from a directory of directories """
-	logger.info("serving requests from %s", ports)
-	i = inotify.adapters.Inotify()
-	for port in Path(ports).iterdir():
-		if not port.is_dir():
-			continue
-		# port_setup(port)
-		todo = port/"todo"
-		logger.info("watching %s", todo)
-		i.add_watch(str(todo), mask=inotify.constants.IN_CREATE | inotify.constants.IN_MOVED_TO)
-	for port in Path(ports).iterdir():
-		if not port.is_dir():
-			continue
-		todo = port/"todo"
-		for req in todo.iterdir():
-			if not req.is_dir():
-				continue
-			process_request(ports, port, req.name, fn)
-	for event in i.event_gen(yield_nones=False):
-		(_, type_names, path, filename) = event
-		logger.debug("PATH=[%r] FILENAME=[%r] EVENT_TYPES=%r", path, filename, type_names)
-		port = Path(path).parent
-		process_request(ports, port, filename, fn)
+    """ Serve requests from a directory of directories """
+    logger.info("serving requests from %s", ports)
+    i = inotify.adapters.Inotify()
+    for port in Path(ports).iterdir():
+        if not port.is_dir():
+            continue
+        # port_setup(port)
+        todo = port/"todo"
+        logger.info("watching %s", todo)
+        i.add_watch(str(todo), mask=inotify.constants.IN_CREATE | inotify.constants.IN_MOVED_TO)
+    for port in Path(ports).iterdir():
+        if not port.is_dir():
+            continue
+        todo = port/"todo"
+        for req in todo.iterdir():
+            if not req.is_dir():
+                continue
+            process_request(ports, port, req.name, fn)
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+        logger.debug("PATH=[%r] FILENAME=[%r] EVENT_TYPES=%r", path, filename, type_names)
+        port = Path(path).parent
+        process_request(ports, port, filename, fn)
 
 
 def setup_logging(verbose, debug):
-	""" Setup logging """
-	log_level = logging.WARNING
-	fmt = "%(message)s"
-	if debug:
-		log_level = logging.DEBUG
-		fmt = "%(asctime)s %(levelname)s %(name)s %(message)s"
-	elif verbose:
-		log_level = logging.INFO
-	logging.basicConfig(level=log_level, format=fmt)
+    """ Setup logging """
+    log_level = logging.WARNING
+    fmt = "%(message)s"
+    if debug:
+        log_level = logging.DEBUG
+        fmt = "%(asctime)s %(levelname)s %(name)s %(message)s"
+    elif verbose:
+        log_level = logging.INFO
+    logging.basicConfig(level=log_level, format=fmt)
 
 
 def main(ports=str(ports_dir), model="medium.en", verbose=False, debug=False):
-	""" main function """
-	setup_logging(verbose, debug)
-	the_model = whisper.load_model(model) if model else None
-	fn = partial(gen, model=the_model)
-	serve_requests(ports, fn)
+    """ main function """
+    setup_logging(verbose, debug)
+    the_model = whisper.load_model(model) if model else None
+    fn = partial(gen, model=the_model)
+    serve_requests(ports, fn)
 
 
 if __name__ == "__main__":
-	try:
-		argh.dispatch_command(main)
-	except KeyboardInterrupt:
-		logger.info("interrupted")
-		sys.exit(1)
+    try:
+        argh.dispatch_command(main)
+    except KeyboardInterrupt:
+        logger.info("interrupted")
+        sys.exit(1)
