42c371796edf6a2a012136b7f127aa5b67b85f6a
Sam Watkins <sam@ucm.dev>
2025-02-17 02:20:35 +1100

    refactor(rubbish): convert rubbish-empty to a function
    - Added verbose option (-v flag)
    - Made script sourceable as a function

--- files/rubbish_empty.sh
+++ files/rubbish_empty.sh
@@ -1,11 +1,33 @@
-#!/bin/bash -e
-# rubbish-empty: empty the rubbish bin
-for BASE in "$HOME" $(qe df | tail -n +2 | awk '{print $6}'); do
-	RUBBISH=$BASE/.rubbish
-	if [ ! -d "$RUBBISH" ]; then
-		continue
-	fi
-	cd "$RUBBISH"
-	find -mindepth 1 -maxdepth 1 -print0 | xargs --no-run-if-empty -0 rm -f -v -r --
-	# rmdir "$RUBBISH"
-done
+#!/usr/bin/env bash
+
+# empty the rubbish bin
+# Empties all .rubbish directories
+
+rubbish-empty() {
+	local verbose= v=  # verbose
+
+	eval "$(ally)"
+
+	local base
+	for base in "$HOME" $(qe df | tail -n +2 | awk '{print $6}'); do
+		local rubbish="$base/.rubbish"
+
+		if [ ! -d "$rubbish" ]; then
+			continue
+		fi
+
+		cd "$rubbish" || continue
+
+		if [ "$verbose" = 1 ]; then
+			printf >&2 "%s\n" "$rubbish"
+		fi
+
+		find . -mindepth 1 -maxdepth 1 -print0 | xargs --no-run-if-empty -0 rm -f -v -r --
+	done
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	rubbish-empty "$@"
+fi
+
+# version: 0.1.1
