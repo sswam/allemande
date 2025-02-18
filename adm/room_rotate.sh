dd30572274ca9920eaa6b4f9bdb642f821ace902
Sam Watkins <sam@ucm.dev>
2025-02-18 22:01:38 +0000

    feat(room-rotate): use numbered room suffixes instead of dates

--- adm/room_rotate.sh
+++ adm/room_rotate.sh
@@ -30,39 +30,19 @@ if [ ! -s "$from" ]; then
 	exit 0
 fi
 
-# Set the destination directory
-dir="$ALLEMANDE_HOME/rooms/$room/"
-
-# Get the modification time of the source file and format it as a date
-secs=`stat -c %Y "$from"`
-date=`date +%Y%m%d -d "@$secs"`
+# Find the lowest unused number
+i=0
+while [ -e "$ALLEMANDE_HOME/rooms/$room-$i.bb" ] || [ -e "$ALLEMANDE_HOME/rooms/$room-$i.html" ]; do
+	i=$((i + 1))
+done
 
 # Set the destination file paths
-to="$dir/$date.bb"
-to_html="$dir/$date.html"
-
-# If destination files already exist, use a timestamp in the filename
-if [ -e "$to" -o -e "$to" ]; then
-	timestamp=`date +%Y%m%d%H%M%S -d "@$secs"`
-	to="$dir/$timestamp.bb"
-	to_html="$dir/$timestamp.html"
-fi
-
-# Check again if the destination files exist
-if [ -e "$to" -o -e "$to" ]; then
-	echo "File exists: $to" >&2
-	exit 1
-fi
-
-# Create the destination directory if it doesn't exist
-mkdir -p "$dir"
+to="$ALLEMANDE_HOME/rooms/$room-$i.bb"
+to_html="$ALLEMANDE_HOME/rooms/$room-$i.html"
 
 # Move the source files to the destination
 mv -n "$from_html" "$to_html"
 mv -n "$from" "$to"
-# sleep 1
-# mv -n "$to_html.orig" "$to_html"
-# rm -f "$to_html.orig"
 
 # Create empty source files
 touch "$from"
