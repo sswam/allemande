170007ec142da1cc89c1165098cb2fac710b0bdc
Sam Watkins <sam@ucm.dev>
2025-02-19 01:26:33 +0000

    feat(rooms): convert rooms_commit to func, add gitignore to rooms.dist

--- adm/rooms_commit.sh
+++ adm/rooms_commit.sh
@@ -2,17 +2,25 @@
 #
 # commit rooms
 
-find "$ALLEMANDE_ROOMS" -type d -name .git | tac | (
-	status=0
-	while read -r git; do
+rooms-commit() {
+	local verbose= v=      # show output even on success
+
+	eval "$(ally)"
+
+	local status=0
+	while IFS= read -r git; do
 		repo=$(dirname "$git")
-		(
-			if cd "$repo" && quiet-on-success arcs; then
-				:
-			else
-				status=1
-			fi
-		)
-	done
-	exit $status
-)
+		if [ "$verbose" = 1 ]; then
+			verbose arcs "$repo" || status=1
+		else
+			quiet-on-success arcs "$repo" || status=1
+		fi
+	done < <(find "$ALLEMANDE_ROOMS" -type d -name .git | tac)
+	return "$status"
+}
+
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	rooms-commit "$@"
+fi
+
+# version: 0.1.1
