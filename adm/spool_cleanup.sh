9f02c8794595f5850bc361d38dbf77854c289eec
Sam Watkins <sam@ucm.dev>
2025-02-26 22:09:57 +0000

    feat(spool_cleanup): extend cleanup to include *-req-* directories

--- adm/spool_cleanup.sh
+++ adm/spool_cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 . get_root
 find /var/spool/allemande/ -name '\.*' -prune -o -type d -name 'req-*' -print | xa move-rubbish
+find /var/spool/allemande/ -name '\.*' -prune -o -type d -name '*-req-*' -print | xa move-rubbish
