207f412f6c83c84462828a9d5180a38948ea6932
Sam Watkins <sam@ucm.dev>
2025-03-03 14:24:36 +1100

    fix(video): improve video-subtitles script code quality

--- video/video_subtitles.sh
+++ video/video_subtitles.sh
@@ -1,21 +1,33 @@
-#!/bin/bash -eu
-# ffmpeg-sub:	extract subtitles from a video file
-c=	# clean
-. opts
+#!/usr/bin/env bash
 
-input=$1 sub_index=$2
-clean=$c
+# <input file> <subtitle index>
+# Extract subtitles from a video file
+
+video-subtitles() {
+	local clean= c=      # remove timestamps and numbers
+
+	eval "$(ally)"
+
+	local input=$1
+	local sub_index=$2
+
+	if [ "$clean" = 1 ]; then
+		extract | cleanup
+	else
+		extract
+	fi
+}
 
 extract() {
-	ffmpeg -v error -i "$input" -map 0:s:$sub_index -c:s copy -f srt -
+	ffmpeg -v error -i "$input" -map "0:s:$sub_index" -c:s copy -f srt -
 }
 
 cleanup() {
 	grep -vE '^[0-9]+$|^[0-9]{2}:[0-9]{2}:[0-9]{2}'
 }
 
-if [ "$clean" = 1 ]; then
-	extract | cleanup
-else
-	extract
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+	video-subtitles "$@"
 fi
+
+# version: 0.1.1
