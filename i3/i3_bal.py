88226e3533a253554d2ad50c3c7543054aa7cf7c
Sam Watkins <sam@ucm.dev>
2025-03-09 09:45:14 +1100

    feat(i3): add recursive container balancing for i3 window sizes
    - Add --all to balance from workspace root down to focused window
    - Add --ALL to balance entire i3 window tree
    - Add proper argument parsing and help text
    - Improve code organization with separate balancing functions

--- i3/i3_bal.py
+++ i3/i3_bal.py
@@ -1,37 +1,87 @@
 #!/usr/bin/env python
-# i3-bal - Balance the size of the windows in the current container
 
+# I'll help modify i3_bal.py to add the --all and --ALL options, fix issues, and update it based on the requirements. Here's the improved version:
+
+"""i3-bal - Balance the size of windows in i3 containers"""
+
+import argparse
 import i3ipc
 
-def main(args):
-    i3 = i3ipc.Connection()
-    tree = i3.get_tree()
 
-    focused = i3.get_tree().find_focused().parent
-
-    num_children = len(focused.nodes)
+def balance_container(container) -> None:
+    """Balance all child windows in a container"""
+    num_children = len(container.nodes)
     if num_children <= 1:
-        print('{} children, doing nothing'.format(num_children))
-        return 0
+        return
 
     new_percentage = int(100 / num_children)
-    if focused.layout == 'splith':
+    if container.layout == "splith":
         ppt_x = new_percentage
         ppt_y = 0
-    elif focused.layout == 'splitv':
+    elif container.layout == "splitv":
         ppt_x = 0
         ppt_y = new_percentage
     else:
-        print('Can\'t resize with {} layout'.format(focused.layout))
-        return 1
+        return
 
     # Add a loop to iterate multiple times to balance window sizes
     for _ in range(4):
-        focused.command_children('resize set {ppt_x} ppt {ppt_y} ppt'.format(ppt_x=ppt_x, ppt_y=ppt_y))
+        container.command_children(f"resize set {ppt_x} ppt {ppt_y} ppt")
+
+
+def balance_up_to_focused(focused) -> None:
+    """Balance containers from workspace root down to focused window's parent"""
+    # Start from workspace root and work down to focused window's parent
+    current = focused.workspace()
+    path = []
+    node = focused.parent
+    while node and node.id != current.id:
+        path.append(node)
+        node = node.parent
+
+    # Balance from top down
+    if current:
+        balance_container(current)
+    for container in reversed(path):
+        balance_container(container)
+
+
+def balance_entire_tree(root) -> None:
+    """Balance every container in the entire tree"""
+    # Process current container
+    balance_container(root)
+
+    # Recursively process all child containers
+    for node in root.nodes:
+        balance_entire_tree(node)
+
+
+def main(argv) -> int:
+    """Main program entry point"""
+    parser = argparse.ArgumentParser(description="Balance i3 window sizes")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-a", "--all", action="store_true",
+                    help="Balance from workspace root to focused window's parent")
+    group.add_argument("-A", "--ALL", action="store_true",
+                    help="Balance entire i3 tree")
+    args = parser.parse_args(argv[1:])
+
+    i3 = i3ipc.Connection()
+    tree = i3.get_tree()
+    focused = tree.find_focused()
+
+    if args.ALL:
+        balance_entire_tree(tree)
+    elif args.all:
+        balance_up_to_focused(focused)
+    else:
+        if not focused.parent:
+            return 0
+        balance_container(focused.parent)
 
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     sys.exit(main(sys.argv))
