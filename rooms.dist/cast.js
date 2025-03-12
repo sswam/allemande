628fa509cbdb12be5bfd47c7376b21f7c7dde54c
Sam Watkins <sam@ucm.dev>
2025-03-12 12:32:26 +0000

    refactor(cast): simplify shuffling by operating on container divs

--- rooms.dist/cast.js
+++ rooms.dist/cast.js
@@ -1,39 +1,18 @@
 function shuffleImages(selector) {
-	// Find the container element
 	const container = document.querySelector(selector);
 	if (!container) return;
 
-	const images = container.querySelectorAll("img");
-
-	// Get all img+div pairs
-	const pairs = [];
-
-	// Group images with their alt divs into pairs
-	for (let img of images) {
-		const altDiv = img.nextElementSibling;
-		if (altDiv && altDiv.classList.contains('alt')) {
-			pairs.push([img, altDiv]);
-		} else {
-			pairs.push([img, null]);
-		}
-	}
+	const divs = Array.from(container.querySelectorAll("div.image"));
 
 	container.innerHTML = "";
 
 	// Fisher-Yates shuffle
-	for (let i = pairs.length - 1; i > 0; i--) {
+	for (let i = divs.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
-		[pairs[i], pairs[j]] = [pairs[j], pairs[i]];
+		[divs[i], divs[j]] = [divs[j], divs[i]];
 	}
 
-	// Reinsert shuffled pairs
-	pairs.forEach(([img, altDiv]) => {
-		container.appendChild(img);
-		if (altDiv) {
-			container.appendChild(altDiv);
-		}
-	});
+	divs.forEach(div => container.appendChild(div));
 }
 
-// Usage:
-shuffleImages('.image_size_1 > p');
+//setTimeout(() => shuffleImages('.cast > p'), 100);
