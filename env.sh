9968a70996ec901329d82c62bc94aa7235c4efc9
Sam Watkins <sam@ucm.dev>
2025-03-13 16:56:29 +0000

    style(env): add css directory to ALLEMANDE_PATH

--- env.sh
+++ env.sh
@@ -57,7 +57,7 @@ for dir in perl; do
 	PERL5LIB=${PERL5LIB:-}:$ALLEMANDE_HOME/$dir
 done
 
-ALLEMANDE_PATH="adm core sys tools text data image audio speech video code llm anthropic chat voice-chat eg www html markup i3 git gradio wordpress python perl scrape misc youtube email prompt unprompted geo subs files bash tty ally plan time bash/tests python/tests perl/tests c markdown x11 apps/story debian richtext refactor math vpn wasm linux"
+ALLEMANDE_PATH="adm core sys tools text data image audio speech video code llm anthropic chat voice-chat eg www html markup i3 git gradio wordpress python perl scrape misc youtube email prompt unprompted geo subs files bash tty ally plan time bash/tests python/tests perl/tests c markdown x11 apps/story debian richtext refactor math vpn wasm linux css"
 
 # TODO only use canon and alias
 # for dir in canon alias; do
