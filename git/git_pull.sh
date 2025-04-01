2d8057cb3652f11a9f62f7e148e605f46f0d09c1
Sam Watkins <sam@ucm.dev>
2025-04-01 05:26:56 +0000

    feat(git): add stash option to git_pull.sh script
    - Adds -s flag to auto-stash/unstash changes during pull

--- git/git_pull.sh
+++ git/git_pull.sh
@@ -1,4 +1,21 @@
-#!/bin/sh -e
-cd "`git-root`"
+#!/bin/bash -e
+
+cd "$(git-root)"
+
+stash=0
+if [ "$1" = "-s" ]; then
+	stash=1
+	shift
+fi
+
+if (( stash )); then
+	git stash
+fi
+
 git pull --rebase "$@"
+
+if (( stash )); then
+	git stash pop
+fi
+
 git status
