34dc51ef68a68ea2747bb10c748017fa7d2c5630
Sam Watkins <sam@ucm.dev>
2025-04-14 17:45:05 +1000

    refactor(config): move server ssh vars, add site urls, clean up defaults - Add sam to admin/moderator lists in dist config. - Move old speak options to snip/.

---
+++ snip/config.sh
@@ -0,0 +1,8 @@
+#speak="speak.py --model coqui:tts_models/en/ek1/tacotron2 --tempo 1.2 --pitch 5 $DEAFEN"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC_ph --tempo 1.6 --pitch 3 $DEAFEN"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC --tempo 1.1 --pitch 3 $DEAFEN"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/glow-tts --tempo 1.2 --pitch 4 $DEAFEN"
+speak="speak.py --model gtts:en:co.uk --tempo 1.2 --pitch 4 $DEAFEN"
+#speak="speak.py --model coqui:tts_models/en/ljspeech/tacotron2-DDC --tempo 1.3 --pitch 4 $DEAFEN"
+
+DEAFEN="--deafen"
