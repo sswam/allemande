ec0173410203c8d9cd8d29b9e231b89fa5ac4ac9
Sam Watkins <sam@ucm.dev>
2025-04-16 06:23:03 +1000

    feat(git): add ppp alias and improve git_pull stash handling - Make git_pull only stash when local changes exist - Tag git_pull stashes and pop the specific tagged stash - Add git_pull_push_pull.sh script

--- git/git_pull.sh
+++ git/git_pull.sh
@@ -9,13 +9,17 @@ if [ "$1" = "-s" ]; then
 fi
 
 if (( stash )); then
-	git stash
+	if git diff-index --quiet HEAD --; then
+		stash=0
+	else
+		git stash push -m "pull-tmp-$$"
+	fi
 fi
 
 git pull --rebase "$@"
 
 if (( stash )); then
-	git stash pop
+	git stash pop "$(git stash list | grep "pull-tmp-$$" | cut -d: -f1)"
 fi
 
 git status
---
+++ git/git_pull_push_pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eu
+git_pull -s
+git_push
+git_pull -s
