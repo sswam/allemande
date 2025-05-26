7c512bb249d282e9b1a99faf4a2563f405321e85
Sam Watkins <sam@ucm.dev>
2025-05-26 14:39:43 +1000

    fix(a1111_kill): Avoid kill attempt if automatic1111 process not found

--- core/a1111_kill.sh
+++ core/a1111_kill.sh
@@ -2,4 +2,7 @@
 # Kill the automatic1111 stable diffusion webui process,
 # if case of failure, so it will restart and hopefully work!
 # Depends on our use of /opt/venvs/python3.10-automatic1111
-v kill $(psgrep automatic1111 | awk '{print $2}') || true
+PID=$(psgrep automatic1111 | awk '{print $2}')
+if [ -n "$PID" ]; then
+	v kill $PID || true
+fi
