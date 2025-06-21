2890853f5399e030b25fbf7137fe8b5d3328661c
Sam Watkins <sam@ucm.dev>
2025-06-21 15:49:27 +0000

    refactor(visual): split photo prompt template into cast_photo.txt
    - Move photo specific prompt details from cast.txt to the new file.
    - Update cast_photos.sh script to use cast_photo.txt.
    - Adjust age prompt weight to 1.6.
    - Add [get clothes_lower] to the simplified cast.txt.

--- visual/cast_photos.sh
+++ visual/cast_photos.sh
@@ -6,6 +6,6 @@ while read char; do
 	mkdir -p "$char"
 	(
 		cd "$char"
-		a1111-client --count "$N" --model "juggernautXL_juggXIByRundiffusion" --cfg-scale 4.5 --pag --adetailer "face_yolov8n.pt" --ad-mask-k-largest 10 --hires 1.5 --prompt "[sets char=\"$char\"] [use cast] [use square] [sets steps=30]"
+		a1111-client --count "$N" --model "juggernautXL_juggXIByRundiffusion" --cfg-scale 4.5 --pag --adetailer "face_yolov8n.pt" --ad-mask-k-largest 10 --hires 1.5 --prompt "[sets char=\"$char\"] [use cast_photo] [use square] [sets steps=30]"
 	)
 done < people.txt
