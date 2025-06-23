1a0434ebed3378873423320bd95abcd5f3990942
Sam Watkins <sam@ucm.dev>
2025-06-23 13:46:27 +0000

    feat(tsv2recs): Control output of undefined fields with --all flag - Use `--all` (`-a`) to include fields with undefined values, output as   `key:`. - By default, undefined fields are now omitted from output. - Ensures all header-defined columns are considered for each row. - Standardizes I/O encoding to `:encoding(UTF-8)`.

--- data/tsv2recs.pl
+++ data/tsv2recs.pl
@@ -1,29 +1,47 @@
 #!/usr/bin/perl -w
+
 use strict;
 use warnings;
 use utf8;
 use open ':utf8';
-use Data::Dumper;
 use Text::TSV;
+use Getopt::Long;
 
+# Process command line options
+my $include_all = 0;
+GetOptions(
+	"all|a" => \$include_all,
+) or die "Error in command line arguments\n";
+
+# Setup input/output handles with proper UTF-8 encoding
 our $__in;
 our $__out;
-$__in = \*STDIN;  binmode($__in, ":utf8");
-$__out = \*STDOUT;  binmode($__out, ":utf8");
+$__in = \*STDIN;  binmode($__in, ":encoding(UTF-8)");
+$__out = \*STDOUT;  binmode($__out, ":encoding(UTF-8)");
+
+# Initialize TSV parser and get header row
 my $__csv_in = Text::TSV->new;
 my $in_names = $__csv_in->getline($__in);
 
+# Process each row of the TSV file
 while (1) {
 	my $__row_in = $__csv_in->getline($__in);
 	if (!$__row_in) {
 		$__csv_in->eof or die $__csv_in->error_diag();
 		last;
 	}
-	for my $i (0..$#$__row_in) {
-		my ($k, $v) = ($in_names->[$i], $__row_in->[$i]);
+
+	# Output fields according to include_all flag
+	for my $i (0..$#$in_names) {
+		my $k = $in_names->[$i];
+		my $v = $__row_in->[$i];
+		# Skip undefined values unless include_all is set
+		next if !$include_all && !defined $v;
+		$v //= '';
 		chomp $v;
 		$v =~ s/^/\t/m;
 		print $__out "$k:$v\n";
 	}
+
 	print $__out "\n";
 }
