60312046d1b5fb2ed23f28d24cd1d08fce024672
Sam Watkins <sam@ucm.dev>
2025-07-11 13:33:28 +0000

    feat(onboarding): Revise new user welcome message
    - The Patreon link is now selected dynamically based on an `nsfw` flag.

--- adm/webchat_user.sh
+++ adm/webchat_user.sh
@@ -46,10 +46,17 @@ add-user() {
 		die "User $user already exists"
 	fi
 
+	local patreon
+	if ((nsfw)); then
+		patreon="https://www.patreon.com/allychatx"
+	else
+		patreon="https://www.patreon.com/allychat"
+	fi
+
 	change-password "$@" |
 	while read -r _ user pass; do
 		cat <<END
-*** WARNING: Ally Chat is a cutting-edge power tool.  It's not a dumbed-down app for chuckle-monkeys.
+=== Welcome to Ally Chat! ===
 
 There are a few rules, but we don't believe in censorship or "AI safety".  You are responsible for your own behaviour and your own safety.
 
@@ -57,33 +64,24 @@ Before you start, please read the story of "Wally".  It's short and it's sad.
 
 Wally joined Ally Chat, but he didn't press the ? button, to read the Intro and use the AI Help!  He certainly didn't read the User Guide.  Wally expected Ally Chat to be simple like ChatGPT, but Ally Chat is a power tool.  Wally got confused right away, and gave up on Ally Chat before he even got started.  Don't be a chuckle-monkey like Wally!
 
-We are building the best damn app on the planet.  Don't miss out.
-
-^^^ Please read that carefully, okay?
-
-
-Okay, then...
-
-=== Welcome to Ally Chat! ===
+*** WARNING: This app is a power tool. It takes a little time to learn.
 
 Login at https://$ALLEMANDE_DOMAIN
 Username: $user
 Password: $pass
 
 Getting Started:
-1. Say "hi" in the "Ally Chat" room!
+1. Say "hi" and chat a little in the "Ally Chat" room.
 2. Press '?' and read the Intro
 3. Visit the AI 'help' tab and ask some questions
 4. Ask Sam for a demo.
 
-If you don't follow these steps, we reserve the right to delete your account!
-
 Please consider joining our Patreon to support the project.
-- https://www.patreon.com/allychat
+- $patreon
 
 Free-tier users have access to all models and features, but subscribers get some good perks too.
 
-We do need your help, if you can. Subscriptions start at \$5 / month.  You can also join as a free member.
+Subscriptions start at \$5 / month.  You can also join as a free member.
 END
 	done
 
