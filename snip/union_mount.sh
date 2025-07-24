00b45e94fed4e7124e26023528590f36e33a8117
Sam Watkins <sam@ucm.dev>
2025-07-24 06:11:48 +0000

    feat(mount): create workdir for overlay mount [snip]

--- snip/union_mount.sh
+++ snip/union_mount.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $ALLEMANDE_HOME
 union-umount
-mkdir -p rooms.beorn
+mkdir -p rooms.beorn rooms.work
 sshfs -o allow_other -p 2222 localhost:rooms/ rooms.beorn
 sudo mount -t overlay overlay -o lowerdir="$ALLEMANDE_HOME/rooms.beorn",upperdir="$ALLEMANDE_HOME/rooms.opal",workdir="$ALLEMANDE_HOME/rooms.work" "$ALLEMANDE_ROOMS"
