8eab01dbc93d957a1d90ba3a30f7370764db7841
Sam Watkins <sam@ucm.dev>
2025-07-27 04:21:41 +1000

    fix(deadlinks): don't fail on none found

--- files/deadlinks.sh
+++ files/deadlinks.sh
@@ -1,8 +1,10 @@
-#!/bin/bash -eu
+#!/bin/bash
 # [dir ...]
 # find dead symlinks
 eval "$(ally)"
+set +e +u
 find "$@" -type l |
 while read L; do
 	[ -L "$L" ] && [ ! -e "$L" ] && echo "$L"
 done
+exit 0
