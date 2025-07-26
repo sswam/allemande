cd844621457ab73ed1991d0445febf1fcf2f4f46
Sam Watkins <sam@ucm.dev>
2025-07-26 05:06:10 +0000

    chore(agents): Rename Jax to Jaxx and Kris to Kriss
    - Consolidate Aetheria visual definition into the character file.
    - Clean up dead symlinks during visual asset installation.
    - Make user vs agent comparison script more robust.

--- adm/webchat_users_vs_agents.sh
+++ adm/webchat_users_vs_agents.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e -u
+cd
 < $ALLYCHAT_HOME/.htpasswd cut -f1 -d: | sort > users.txt
 (cd $ALLEMANDE_AGENTS ; find . -name '*.yml' | grep -v '/\.' | sed 's/\.yml$//; s/.*\///') | lc | sort > agents.txt
-< ~/rooms/.agents_global.yml sed 's/^  - //' | lc | sort > agents_loaded.yml
+< ~/rooms/.agents_global.yml sed 's/^  - //' | lc | sort > agents_loaded.txt
 
 echo "## virtual agents"
-comm -3 agents.txt agents_loaded.yml 
+comm -3 agents.txt agents_loaded.txt 
 echo
 
 echo "## user / agent conflicts"
-comm -1 -2 agents_loaded.yml users.txt
+comm -1 -2 agents_loaded.txt users.txt
 echo
