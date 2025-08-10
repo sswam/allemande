c20bd0d67eaa97a71280fc084bb045ec6c69cb76
Sam Watkins <sam@ucm.dev>
2025-08-10 12:27:09 +0000

    feat(doc): compile agent summaries from generated files
    - Replace glob with `find` to sort files alphabetically.

--- doc/summaries_compile.sh
+++ doc/summaries_compile.sh
@@ -5,8 +5,8 @@ while read dir; do
 	echo
 	(
 		cd "$ALLEMANDE_AGENTS/$dir"
-		for A in *.yml; do
-			A=${A%.yml}
+		find . -name '*.yml' | sort | while read -r file; do
+			A=$(basename "$file" .yml)
 			echo -n "- $A - "
 			cat "$ALLEMANDE_HOME/doc/summary/$A.txt" || echo
 		done
