fb3cef0771a0d1b27d7d77714bbedd1c64cdee9f
Sam Watkins <sam@ucm.dev>
2025-08-10 12:30:43 +0000

    feat(mount): Mount extra rooms directory via sshfs
    - Creates the mount point if it doesn't exist.
    - Unmounts the directory if it's already mounted.

--- adm/mount_extra.sh
+++ adm/mount_extra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 cd "$ALLEMANDE_HOME"
-mkdir -p rooms.extra
+mkdir -p rooms.extra || true
 fusermount -u rooms.extra || true
 sshfs -o allow_other -p 2222 localhost:rooms/ rooms.extra
