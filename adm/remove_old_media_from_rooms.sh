58d1f307b9cd3eba9f7bc1ad2f82776df6195db1
Sam Watkins <sam@ucm.dev>
2025-08-13 11:00:54 +0000

    feat(adm): clean up old png, jpeg, and mp4 files, not just jpg
    - The script was renamed from `remove_old_images_from_rooms.sh` to
      `remove_old_media_from_rooms.sh` to reflect its expanded scope.

--- adm/remove_old_media_from_rooms.sh
+++ adm/remove_old_media_from_rooms.sh
@@ -11,13 +11,13 @@ cd "$dir"
 confirm -d=n "Please confirm the following details: $HOSTNAME, $PWD, $days days?" || exit 1
 
 # Get a list of all jpg files older than $days days, not in the cast directory, and are owner writable
-find "." -mtime +$days -type f -name '*.jpg' -not -path '*/cast/*' -perm /200 | sed 's|^\./||' | sort > .all_jpg_files.txt
+find "." -mtime +$days -type f \( -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' -o -name '*.mp4' \) -not -path '*/cast/*' -perm /200 | sed 's|^\./||' | sort > .all_media_files.txt
 
 # Get list of files tracked by Git
 # NOTE: doesn't handle sub-repositories
-git ls-files | grep '\.jpg$' || true | sed 's|^\./||' | sort > .git_jpg_files.txt
+git ls-files | grep -E '\.(jpe?g|png|mp4)$' || true | sed 's|^\./||' | sort > .git_media_files.txt
 
 # Use comm to find files that are in all_jpg_files but not in git_jpg_files, and remove them
-comm -23 .all_jpg_files.txt .git_jpg_files.txt | xargs-better move-rubbish --
+comm -23 .all_media_files.txt .git_media_files.txt | xargs-better move-rubbish --
 
-rm -f .all_jpg_files.txt .git_jpg_files.txt
+rm -f .all_media_files.txt .git_media_files.txt
