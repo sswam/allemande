0f067c8bb6646b027616499138916a82c73e1d20
Sam Watkins <sam@ucm.dev>
2025-08-29 04:13:47 +0000

    fix(adm): make spool-cleanup faster

--- adm/spool_cleanup.sh
+++ adm/spool_cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . get_root
-find /var/spool/allemande/ -name '\.*' -prune -o -type d -name 'req-*' -print | xa move-rubbish
-find /var/spool/allemande/ -name '\.*' -prune -o -type d -name '*-req-*' -print | xa move-rubbish
+find /var/spool/allemande/ -name '\.*' -prune -o -type d -name 'req-*' -print | xa q move-rubbish
+find /var/spool/allemande/ -name '\.*' -prune -o -type d -name '*-req-*' -print | xa q move-rubbish
