11bf7a934344df1bd11490220c4a60f2b96581e5
Sam Watkins <sam@ucm.dev>
2025-08-30 17:22:23 +0000

    fix: Unmount rooms.extra before mounting it
    - Avoids errors if the mountpoint is already in use.

--- adm/mount_extra.sh
+++ adm/mount_extra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 cd "$ALLEMANDE_HOME"
 mkdir -p rooms.extra || true
+pkill -f "sshfs.*rooms.extra" || true
 fusermount -u rooms.extra || true
 # sshfs -o allow_other -p 2222 localhost:rooms/ rooms.extra
 # TODO not hard code VPN IP address
