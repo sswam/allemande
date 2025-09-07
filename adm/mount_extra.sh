3b863ed177f09f4678d90cba808f593fed1545a5
Sam Watkins <sam@ucm.dev>
2025-09-07 07:32:27 +0000

    fix(adm): update IP for rooms.extra sshfs mount
    Changes the hardcoded VPN IP address from 10.0.0.2 to 10.0.1.50.

--- adm/mount_extra.sh
+++ adm/mount_extra.sh
@@ -5,4 +5,4 @@ pkill -f "sshfs.*rooms.extra" || true
 fusermount -u rooms.extra || true
 # sshfs -o allow_other -p 2222 localhost:rooms/ rooms.extra
 # TODO not hard code VPN IP address
-sshfs -o allow_other 10.0.0.2:rooms/ rooms.extra
+sshfs -o allow_other 10.0.1.50:rooms/ rooms.extra
