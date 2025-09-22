6c99813bf8b169850adc4f90678cf0f2653a0bec
Sam Watkins <sam@ucm.dev>
2025-09-22 13:46:19 +0000

    feat(mount_extra): add raw and cache dirs, boost sshfs caching options
    - WIP: I need to write my own fuse fs for read-only caching, see fs/
    
    - Create rooms.extra.raw and rooms.extra.cache directories for additional
      mounting.
    - Add fusermount unmount for rooms.extra.raw.
    - Update sshfs invocation with extended kernel and dcache caching options for
      improved performance and file system latency reduction.
    - Add comment for potential use of mcachefs layering on rooms.extra.

--- adm/mount_extra.sh
+++ adm/mount_extra.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -eu
 cd "$ALLEMANDE_HOME"
-mkdir -p rooms.extra || true
-pkill -f "sshfs.*rooms.extra" || true
+mkdir -p rooms.extra rooms.extra.raw rooms.extra.cache || true
 fusermount -u rooms.extra || true
+pkill -f "sshfs.*rooms.extra" || true
+fusermount -u rooms.extra.raw || true
 # sshfs -o allow_other -p 2222 localhost:rooms/ rooms.extra
 # TODO not hard code VPN IP address
-sshfs -o allow_other 10.0.1.50:rooms/ rooms.extra
+
+# sshfs -o allow_other 10.0.1.50:rooms/ rooms.extra
+
+sshfs -o allow_other -o kernel_cache,noforget,remember=86400,dcache_timeout=86400,dcache_stat_timeout=86400,dcache_link_timeout=86400,dcache_dir_timeout=86400,entry_timeout=86400,attr_timeout=86400,negative_timeout=86400,dcache_max_size=100000 10.0.1.50:rooms/ rooms.extra # rooms.extra.raw
+# mcachefs -o cache=rooms.extra.cache,metafile=rooms.extra.meta,journal=rooms.extra.journal rooms.extra.raw rooms.extra
