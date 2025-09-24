9dac7f7d16bf2337f6660eac696e134cf77b6bca
Sam Watkins <sam@ucm.dev>
2025-09-24 17:25:13 +1000

    fix(tools): fix terminal title prefix order and priority

--- tools/name_terminal_nicely.sh
+++ tools/name_terminal_nicely.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 title=""
-if [ -n "$SSH_TTY" ]; then
-    title+="$HOSTNAME: "
-fi
-if [ -n "$STY" ]; then
-    title+="${STY#*.}: "
-fi
 if [ -n "$TERMINAL_TITLE" ]; then
     title+="$TERMINAL_TITLE: "
 fi
+
+if [ -z "$title" ]; then
+    if [ -n "$STY" ]; then
+        title="${STY#*.}: $title"
+    fi
+    if [ -n "$SSH_TTY" ]; then
+        title="$HOSTNAME: $title"
+    fi
+fi
+
 title+="$*"
 title=${title%: }
+
 name-terminal "$title"
