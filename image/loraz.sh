492887ebf885006e5258a387672be49dccdd4cfa
Sam Watkins <sam@ucm.dev>
2025-09-27 04:53:10 +0000

    feat(lora): Split LoRA lists into SFW and NSFW
    - Makefile now generates separate `lora.bb` (SFW) and `lora-nsfw.bb` (NSFW)
      files.
    - The `loraz` script can now search SFW-only, NSFW-only, or both LoRA lists
      based on a new flag.

--- image/loraz.sh
+++ image/loraz.sh
@@ -4,12 +4,21 @@
 # Show LoRAs from ~/lora.bb by search query, one heading per query
 
 loraz() {
+	nsfw= n=0  # include NSFW loras
+
 	eval "$(ally)"
+
+	files=("$HOME/lora.bb")
+	if [ "$nsfw" = 1 ]; then
+		files+=("$HOME/lora-nsfw.bb")
+	elif [ "$nsfw" = 2 ]; then
+		files=("$HOME/lora-nsfw.bb")
+	fi
 
 	for query; do
 		query=${query,,}
 		printf "## %s\n" "$query"
-		grep -i -e "$query" -e "^| <lora:name:weight>" -e '^|--|' -- ~/lora.bb
+		cat "${files[@]}" | uniqo | grep -i -e "$query" -e "^| <lora:name:weight>" -e '^|--|'
 		echo
 	done
 }
