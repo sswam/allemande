3a9d177cbaf7bfb19a398568ed36bf4998e4f096
Sam Watkins <sam@ucm.dev>
2025-10-02 17:47:40 +1000

    chore(tools): improve argument handling and build script
    - `arg1toN.pl` now correctly preserves command-line options, including the
      `--` separator, when reordering arguments.
    - The `install` target in `visual/Makefile` now uses `find` to automatically
      discover and link text files, replacing the hardcoded list.

--- tools/arg1toN.pl
+++ tools/arg1toN.pl
@@ -1,9 +1,32 @@
 #!/usr/bin/perl
+# NOTE: this will preserve options without separate arguments, and all options before --
+# It will not work with options having separate arguments, and no --, as that is impossible without knowing the tool.
+# Allemande shell tools use -f= for that reason.
 use strict;
 use warnings;
 my $prog = shift;
-my @args = @ARGV;
-push @args, shift @args;
-exec $prog, @args;
+my @opts = ();
+
+my $dashdash_ix;
+for (my $i = 0; $i < @ARGV; $i++) {
+	if ($ARGV[$i] eq '--') {
+		$dashdash_ix = $i;
+		last;
+	}
+}
+
+if (defined $dashdash_ix) {
+	push @opts, splice(@ARGV, 0, $dashdash_ix);
+	shift @ARGV; # remove --
+} else {
+	while (@ARGV && $ARGV[0] =~ /^-/) {
+		push @opts, shift @ARGV;
+	}
+}
+if (!@ARGV) {
+	die "Usage: $0 program [options] arg1 [arg2 ...]";
+}
+push @ARGV, shift @ARGV;
+exec $prog, @opts, @ARGV;
 
 # todo skip options? or a version that does
