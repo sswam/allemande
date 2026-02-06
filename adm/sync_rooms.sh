b366859ef69776c03b6c6ee68fa4df2b957c867f
Sam Watkins <sam@ucm.dev>
2026-02-06 16:02:45 +1100

    fix(sync_rooms): add '|| true' to git commands for error tolerance
    Prevents script exit on git restore or pull failures, allowing subsequent
    rsync operations to proceed.

--- adm/sync_rooms.sh
+++ adm/sync_rooms.sh
@@ -2,9 +2,9 @@
 set -e -u -o pipefail
 
 # v opal -- ally-commit -v -N
-v i rooms/ git restore --staged .
-v i rooms/ git restore .
-v i rooms/ git pull ucm.dev:$ALLEMANDE_HOME/rooms
+v i rooms/ git restore --staged . || true
+v i rooms/ git restore . || true
+v i rooms/ git pull ucm.dev:$ALLEMANDE_HOME/rooms || true
 
 # TODO do this more safely
 time v sudo rs --exclude="*.html" ucm.dev:$ALLEMANDE_HOME/rooms/ rooms/ || true
