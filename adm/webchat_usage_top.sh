e1b54d0b1bb0e0822abb1defa4d2159ed605207a
Sam Watkins <sam@ucm.dev>
2026-04-10 13:49:49 +0000

    fix(adm): wut (webchat-usage-top), show total usage and active user count

--- adm/webchat_usage_top.sh
+++ adm/webchat_usage_top.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -eu
 cd "$ALLEMANDE_ROOMS"
 month="${1:-$(date +%Y-%m)}"
-for file in */usage.$month.log; do
-	user=$(dirname "$file")
-	echo -n "$user"$'\t'
-	<"$file" kut 9 | total
-done |
-order 2rn
+list=$(
+	for file in */usage.$month.log $ALLEMANDE_HOME/usage_unknown.$month.log; do
+		user=$(dirname "$file")
+		echo -n "$user"$'\t'
+		<"$file" kut 9 | total
+	done |
+	order 2rn
+)
+(
+	printf "%s" "$list" | wc -l
+	printf "%s" "$list" | kut 2 | total
+	printf "%s" "$list"
+) | less
