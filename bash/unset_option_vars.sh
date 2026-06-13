e848eea22af2e018378125392b54c0910929b429
Sam Watkins <sam@ucm.dev>
2026-06-13 14:01:48 +0000

    feat(chat/tools): add -p flag to web_text for local proxy
    - preserve http_proxy and https_proxy in unset-vars
    - set https_proxy to 127.0.0.1:3128 when -p is first arg
    - update Web.yml description

--- bash/unset_option_vars.sh
+++ bash/unset_option_vars.sh
@@ -2,8 +2,12 @@ function unset-vars() {
     local line varname
     while read -r line; do
         # Match lowercase vars or single uppercase letter vars
+        # exceptions: http_proxy https_proxy
         if [[ $line =~ ^[a-z][a-zA-Z0-9_]*= || $line =~ ^[A-Z]= ]]; then
             varname=${line%%=*}
+            if [ "$varname" = http_proxy -o "$varname" = https_proxy ]; then
+                continue
+            fi
             unset "$varname"
         fi
     done < <(set)
--- chat/tools/web_text.sh
+++ chat/tools/web_text.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -eu
+if [ "${1:-}" == -p ]; then
+	shift
+	export https_proxy="https://127.0.0.1:3128"
+fi
 for url; do
 	web-text -- "$url"
 	echo
