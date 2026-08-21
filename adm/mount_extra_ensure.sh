a5450efca510269fe0ab710d9bdf6d1d256b0de5
Sam Watkins <sam@ucm.dev>
2026-08-21 09:17:11 +0000

    fix(adm): improve mount-extra-ensure mounted check

--- adm/mount_extra_ensure.sh
+++ adm/mount_extra_ensure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 count=$(timeout 10s sh -c 'ls /opt/allemande/rooms.extra | wc -l')
-if [ -z "$count" -o "$count" = 0 ]; then
-	q mount-extra
+if [ -z "$count" -o "$count" = 0 ] || ! mountpoint -q /opt/allemande/rooms.extra || ! mountpoint -q /opt/allemande/rooms.extra.raw; then
+	qos mount-extra
 fi
