a20e3d1a95c2574f69d8aaedebf94ba7d3d0587c
Sam Watkins <sam@ucm.dev>
2026-09-09 22:46:00 +0000

    feat(adm): webchat-user-info, edit option

--- adm/webchat_user_info.sh
+++ adm/webchat_user_info.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# [-e] username*
+# View or edit web user info.rec files
+
+edit= e= # edit rather than view
+
+eval "$(ally)"
+
 for user; do
-	cat "$ALLEMANDE_USERS/$user/info.rec"
+	file="$ALLEMANDE_USERS/$user/info.rec"
+	if [ -n "$edit" ]; then
+		vim "$file"
+	else
+		cat "$file"
+	fi
 done
